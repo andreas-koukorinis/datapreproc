aa39941026ec341dac4667d9e469ffe7b41add02
SanchitGupta <sanchit.gupta@tworoads.co.in>
2014-09-16 15:23:56 +0530

    Added comments

--- getPrice.py
+++ getPrice.py
@@ -2,15 +2,18 @@
 import MySQLdb
 import sys
 
+#Returns an n*2 array of (Dates,Prices) corresponding to symbol 'product' between dates 'startdate' and 'enddate'
+#The price table's name is assumed to be the product symbol with the rightmost digits removed (ES for ES1,ES2) 
 def getPrice(product,startdate,enddate):
     sym = product.rstrip('1234567890')
     db = MySQLdb.connect(host="fixed-income.clmdxgxhslqn.us-east-1.rds.amazonaws.com", user="cvmysql",passwd="fixedcvincome", db="risk_parity") 
     cur = db.cursor() 
     query = "SELECT Date,"+product+" FROM "+sym+" WHERE Date >= '"+startdate+"' AND Date <= '"+enddate+"'"
     cur.execute(query)
-    printPrices(cur)
+#    printPrices(cur)
     return cur.fetchall()
 
+#prints the dates and prices corresponding to the 'cur' database object
 def printPrices(cur):
     for row in cur.fetchall() :
         st = ""
@@ -19,4 +22,4 @@ def printPrices(cur):
         print(st)
 
 #getPrice('TY1','20140901','20140905')
-getPrice(sys.argv[1],sys.argv[2],sys.argv[3])
+#getPrice(sys.argv[1],sys.argv[2],sys.argv[3])
