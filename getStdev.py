335d95cbcf7639122261d745cf497ba2f4d611d2
SanchitGupta <sanchit.gupta@tworoads.co.in>
2014-09-16 15:25:40 +0530

    Added basic getStdev function

---
+++ getStdev.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+from numpy import *
+
+# Returns the simple Standard deviation of 'returns' ndarray
+def getSimpleStdev(returns):
+    return 1/std(returns,axis=0)	
+
+#To be completed
+def getExponentialStdev(returns):
+    ###
+
