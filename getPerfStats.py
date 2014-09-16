14e3249c04c76fd48c8ed0085c800f00536d3b48
SanchitGupta <sanchit.gupta@tworoads.co.in>
2014-09-16 15:23:45 +0530

    Added comments

--- getPerfStats.py
+++ getPerfStats.py
@@ -2,6 +2,9 @@
 
 from numpy import *
 
+# Returns the performance statistics for the return series 'returns' containing periodic returns with a period of 'rebalance_freq'
+# returns : n*k array containing log returns of n days for k instruments
+# rebalance_freq : the periodicity of returns
 def getPerfStats(returns,rebalance_freq):
     net_log_returns = sum(returns)
     net_percent_returns = (exp(net_log_returns)-1)*100
@@ -18,4 +21,4 @@ def getPerfStats(returns,rebalance_freq):
     return_dd_ratio_percent = annualized_percent_returns / max_dd_percent
     print "\nNet_Log_Returns = %.10f \nNet_Percent_Returns = %.10f%%\nAnnualized_Returns = %.10f%% \nAnnualized_Std = %.10f%% \nSharpe_Ratio = %.10f \nMax_dd = %.10f%% \nReturn_dd_Ratio = %.10f \n" %(net_log_returns,net_percent_returns,annualized_percent_returns,annualized_percent_std,sharpe_percent,max_dd_percent,return_dd_ratio_percent)
 
-getPerfStats(array([log(1.02),log(0.99),log(0.98),log(0.97),log(1.03)]),5)
+#getPerfStats(array([log(1.02),log(0.99),log(0.98),log(0.97),log(1.03)]),5)
