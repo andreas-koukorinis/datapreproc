a785705a86e8a42b14b6193e7caf4186680a4aee
SanchitGupta <sanchit.gupta@tworoads.co.in>
2014-09-17 14:57:07 +0530

    Added getModifiedPrice function

--- init.py
+++ init.py
@@ -1,6 +1,7 @@
 import csv
 from numpy import *
 from getData import getPrice,getSpec
+import MySQLdb
 
 # read log returns data directly from csv file
 def load_data(file_,typ):
@@ -15,7 +16,7 @@ def load_data(file_,typ):
 
 # Computes Log returns for symbol 'product' from 'startdate' to 'enddate'
 # Fetches the (dates,prices) of the products with symbols mentioned in the 'products' list
-# If the symbol contains numeric at the end,then uses specific symbol to calculate the correct returns from prices otherwise simply calculates returns
+# ASSUMPTION : If the symbol contains numeric at the end,then uses specific symbol to calculate the correct returns from prices otherwise simply calculates returns
 # Passes returns and dates to filter_returns function to remove non-common entries among different products
 def getLogReturns(products,startdate,enddate):
     all_dates=[]
@@ -39,6 +40,8 @@ def getLogReturns(products,startdate,enddate):
 # all_dates : list of lists where each list contains the dates for which data is available for a particular instrument
 # all_returns : list of lists where each list contains the returns for the corresponding list in 'all_dates' for a particular instrument
 def filter_returns(all_dates,all_returns):
+    if(len(all_returns)==1):
+        return (array(all_returns).T).astype(float)
     intersected_dates = list(set(all_dates[0]).intersection(*all_dates))
     filteredret = []
     for i in xrange(0,len(all_returns)):
@@ -46,6 +49,16 @@ def filter_returns(all_dates,all_returns):
         filteredret.append(all_returns[i][Indexes])
     filteredret = (array(filteredret).T).astype(float)
     return filteredret
+
+#Returns 2 arrays of (Dates,ModifiedPrices) corresponding to symbol 'product' between dates 'startdate' and 'enddate'
+def getModifiedPrice(product,startdate,enddate):
+    ret = getLogReturns([product],startdate,enddate)    
+    (dates,prices) = getPrice(product,startdate,enddate) 
+    modified_prices = zeros(prices.shape[0])
+    modified_prices[modified_prices.shape[0]-1] = prices[modified_prices.shape[0]-1]
+    modified_prices[0:modified_prices.shape[0]-1] = prices[1:modified_prices.shape[0]]/exp(ret[:,0])
+    print ret[:,0]
+    return (dates,modified_prices)
         
 # Compute Returns based on prices and Specific Symbol (data is filtered beforehand based on common dates)
 # Prices : should be n*k 2d array where n is the number of trading days and k is the number of instruments
@@ -68,3 +81,4 @@ def compute_returns_specs(prices,specific):
     return returns
 
 #getLogReturns(['TY1','ES1'],'20140901','20140905')
+#(d,m) = getModifiedPrice('TY1','20140801','20140830')
