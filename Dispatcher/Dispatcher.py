9da51e3153db61b3f8de2b973d4108f67182dd7e
Gaurav Chakravorty <gchak@circulumvite.com>
2014-10-11 18:45:40 -0400

    ~

--- Dispatcher/Dispatcher.py
+++ Dispatcher/Dispatcher.py
@@ -5,7 +5,7 @@ import ConfigParser
 from Utils.DbQueries import db_connect,db_close,check_settlement_day
 from Utils.Regular import getdtfromdate
 
-#The job of the dispatcher is
+#The job of the dispatcher is :
 #1)To maintain a heap of all the event sources keyed by the timestamp(datetime object in python)
 #Listeners : OnDailyEventUpdate -> BookBuilders ,        OnEventsUpdate -> Strategy,PerformanceTracker
 #2)For each daily event call OnDailyEventUpdate on its listeners corresponding to the product for which the daily event has occured
@@ -22,16 +22,16 @@ class Dispatcher (object):
         config.readfp(open(config_file,'r'))
         start_date = config.get('Dates','start_date')
         end_date = config.get('Dates','end_date')
-        self.start_dt = getdtfromdate(start_date)                                               #Convert date to datetime object with time hardcoded as 23:59:59:999999
+        self.start_dt = getdtfromdate(start_date) #Convert date to datetime object with time hardcoded as 23:59:59:999999
         self.end_dt = getdtfromdate(end_date)
         self.trading_days=0
         warmupdays = config.getint('Parameters','warmupdays')
         self.strategy_start_dt = self.start_dt+datetime.timedelta(days=warmupdays)
         self.products = products
-        self.heap = []										#Initialize the heap,heap will contain tuples of the form (timestamp,event)
-        (self.dbconn,self.db_cursor) = db_connect()                                             #Initialize the database cursor
-        self.eventlisteners = []                                                                #These are the listeners which receive 1 daily event for their product.Here bookbuilders
-        self.eventslisteners = []                                                               #These are the listeners which receive all the concurrent events at once.Here Strategy and Performance Tracker
+        self.heap = []	#Initialize the heap, heap will contain tuples of the form (timestamp,event)
+        (self.dbconn, self.db_cursor) = db_connect() #Initialize the database cursor
+        self.eventlisteners = []  #These are the listeners which receive 1 daily event for their product.Here bookbuilders
+        self.eventslisteners = [] #These are the listeners which receive all the concurrent events at once.Here Strategy and Performance Tracker
 
     @staticmethod
     def GetUniqueInstance(products,config_file):
@@ -40,42 +40,42 @@ class Dispatcher (object):
             Dispatcher.instance.append(new_instance)
         return Dispatcher.instance[0]
 
-    def AddEventListener(self,listener):                                                        #For Bookbuilders
+    def AddEventListener(self,listener): #For Bookbuilders
         self.eventlisteners.append(listener)
 
-    def AddEventsListener(self,listener):                                                       #For strategy and Performance Tracker
+    def AddEventsListener(self,listener): #For strategy and Performance Tracker
         self.eventslisteners.append(listener)
 
-    #Main function which loops over the events and makes appropriate calls
-    #ASSUMPTION:All ENDOFDAY events have same time
+    # Main function which loops over the events and makes appropriate calls
+    # ASSUMPTION:All ENDOFDAY events have same time
     def run(self):
         self.heap_initialize(self.products)							#Add 1 earliest event after the startdate from each source
-        current_dt = heapq.nsmallest(1,self.heap)[0][0]                                         #Get the lowest timestamp which has not been handled
-        while(current_dt<=self.end_dt):                                                         #While timestamp does not surpass the end date
+        current_dt = heapq.nsmallest(1,self.heap)[0][0] #Get the lowest timestamp which has not been handled
+        while(current_dt<=self.end_dt): #While timestamp does not surpass the end date
             concurrent_events=[]
-            while(len(self.heap)>0 and heapq.nsmallest(1,self.heap)[0][0]==current_dt):         #Add all the concurrent events for the current_dt to the list concurrent_events
+            while(len(self.heap)>0 and heapq.nsmallest(1,self.heap)[0][0]==current_dt): #Add all the concurrent events for the current_dt to the list concurrent_events
                 tup = heapq.heappop(self.heap)
                 event = tup[1]
                 concurrent_events.append(event)
             for event in concurrent_events:
-                if(event['type']=='ENDOFDAY'):                                                  #This is an endofday event
+                if(event['type']=='ENDOFDAY'): #This is an endofday event
                     for listener in self.eventlisteners:
                         if(listener.product==event['product']):
-                            listener.OnDailyEventUpdate(event)                                  #call dailybookbuilder to update the book
+                            listener.OnDailyEventUpdate(event) #call dailybookbuilder to update the book
 
                     self.pushdailyevent(event['product'],current_dt+datetime.timedelta(days=1)) #Push the next daily event for this product
 
-                if(event['type']=='INTRADAY'):                                                  #This is an intraday event
-                    pass                                                                        #TO BE COMPLETED:call intradaybookbuilder and push next
-            #assert self.strategy.portfolio.get_portfolio()['cash']>=0                          #After every set of concurrent events,portfolio cash should be non negative
+                if(event['type']=='INTRADAY'): #This is an intraday event
+                    pass          #TO BE COMPLETED:call intradaybookbuilder and push next
+                #assert self.strategy.portfolio.get_portfolio()['cash']>=0 #After every set of concurrent events,portfolio cash should be non negative
 
-            if(len(concurrent_events)>0 and current_dt >= self.strategy_start_dt):              #if there are some events and warmupdays are over
+            if(len(concurrent_events)>0 and current_dt >= self.strategy_start_dt): #if there are some events and warmupdays are over
                 for listener in self.eventslisteners:
-                    listener.OnEventsUpdate(concurrent_events)                                  #Make 1 call to OnEventsUpdate of the strategy and Performance Tracker for all the concurrent events
+                    listener.OnEventsUpdate(concurrent_events) #Make 1 call to OnEventsUpdate of the strategy and Performance Tracker for all the concurrent events
                 self.trading_days=self.trading_days+1
 
             if(len(self.heap)>0):
-                current_dt = heapq.nsmallest(1,self.heap)[0][0]                                 #If the are still elements in the heap,update the timestamp to next timestamp
+                current_dt = heapq.nsmallest(1,self.heap)[0][0] #If the are still elements in the heap,update the timestamp to next timestamp
 
         db_close(self.dbconn)									#Close database connection
 
@@ -85,8 +85,7 @@ class Dispatcher (object):
         #Push DB EOD sources
         for product in products:
             self.pushdailyevent(product,self.start_dt)
-        #Push FLAT FILE sources
-        #TO BE DONE
+        # TODO {} Push FLAT FILE sources
 
     #given a product name and a timestamp(dt),fetch the next eligible ENDOFDAY event from the database
     #Settlement day for each future roduct is tracked,so that Daily log returns can be calculated properly and shifting on settlement day can be done
@@ -106,7 +105,7 @@ class Dispatcher (object):
         try:
             query = "SELECT Date,"+product.lstrip('f')+" FROM "+table+" WHERE Date >= '"+str(dt.date())+"' ORDER BY Date LIMIT 1"
             self.db_cursor.execute(query)
-            data = self.db_cursor.fetchall()                                                      #should check if data exists or not
+            data = self.db_cursor.fetchall() #should check if data exists or not
             return (str(data[0][0]),float(data[0][1]))
         except:
             sys.exit("Error In DB.fetchnext")
