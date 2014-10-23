d8aff02ebb32ffc1baa6385ac5c01e64c66d33a0
Sanchit Gupta <sanchit.gupta@tworoads.co.in>
2014-10-23 23:27:16 +0000

    Deleted GenModelData folder

--- GenModelData/Indicators/Indicator_Listeners.py
+++
@@ -1,7 +0,0 @@
-from abc import ABCMeta,abstractmethod
-
-class DailyLogReturnsListener:
-    __metaclass__=ABCMeta
-
-    @abstractmethod
-    def on_daily_log_returns_update(self,product,daily_log_returns): pass
