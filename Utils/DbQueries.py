8d3b5b77fd9553168bf63434e80e53a0903cd5d3
gchak_gchaklinux <gchak@circulumvite.com>
2014-11-06 15:57:37 -0500

    TODO: throw meaningful error in database operations

--- Utils/DbQueries.py
+++ Utils/DbQueries.py
@@ -34,11 +34,14 @@ def conv_factor(products):
         symbol = product.rstrip('0123456789').lstrip('f')
         query = "SELECT factor,currency FROM tick_conversion WHERE symbol='"+symbol+"'"
         db_cursor.execute(query)
-        data = db_cursor.fetchall()                                                                                      #should check if data exists or not
+        data = db_cursor.fetchall()                                                                                      
+        #TODO{} should check if data exists or not
+        #else throw / print a meaningful error
         (tick_factor_product,currency_product) = (float(data[0][0]),str(data[0][1]))
         query = "SELECT factor from currency_conversion WHERE currency='"+currency_product+"'"
         db_cursor.execute(query)
-        currency_factor_product = float(db_cursor.fetchall()[0][0])                                                      #should check if data exists or not
+        currency_factor_product = float(db_cursor.fetchall()[0][0])                                                      
+        #TODO{} should check if data exists or not
         conv_factor[product] = tick_factor_product*currency_factor_product
     db_close(db)
     return conv_factor
