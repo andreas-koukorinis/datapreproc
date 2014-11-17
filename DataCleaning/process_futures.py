be2ab87de2f02ab6cfddd30c403e39ba6ab58a4e
Sanchit Gupta <sanchit.gupta@tworoads.co.in>
2014-11-17 00:56:09 +0000

    Changed method to get label and start date

--- DataCleaning/process_futures.py
+++ DataCleaning/process_futures.py
@@ -18,8 +18,21 @@ def get_exchange_specific(YYMM):
     YY,MM = YYMM[0:2],YYMM[2:4]
     month_codes = {'01':'F','02':'G','03':'H','04':'J','05':'K','06':'M','07':'N','08':'Q','09':'U','10':'V','11':'X','12':'Z'}
     return month_codes[MM]+YY
-    
+
+def get_YYMM_from_exchange_code( code ):
+    month_codes = {'F':'01','G':'02','H':'03','J':'04','K':'05','M':'06','N':'07','Q':'08','U':'09','V':'10','X':'11','Z':'12'}
+    return code[1:]+month_codes[code[0]]
+
+def get_start_date(product,label,exchange_symbol_manager,start_date):
+    exch_code = get_exchange_specific(label)
+    _basename = product.rsplit('_',1)[0]
+    _req_code = _basename+exch_code
+    while exchange_symbol_manager.get_exchange_symbol(start_date,product) != _req_code:
+        start_date += timedelta(days=1)
+    return start_date
+
 def process_futures(num_contracts,product,to_name,folder):
+    
     exchange_symbol_manager = ExchangeSymbolManager()
     path = '/apps/data/csi/historical_futures'+folder+'/'
     output_path = '/home/cvdev/stratdev/DataCleaning/Data/'
@@ -46,12 +59,14 @@ def process_futures(num_contracts,product,to_name,folder):
 
     sorted_labels = sorted(input_dfs.keys(),key = convert_to_year_month)
     print sorted_labels
-    start_date = input_dfs[sorted_labels[0]].index.values[0]
+    start_date = get_start_date(generic_to_name[0],sorted_labels[0],exchange_symbol_manager,input_dfs[sorted_labels[0]].index.values[0])
+    print start_date
     end_date = datetime.strptime('2014-10-31', "%Y-%m-%d").date()
     delta = end_date-start_date
-    current_idx = 0
     for i in range(delta.days + 1):
-        current_date = start_date + timedelta(days=i)  
+        current_date = start_date + timedelta(days=i)
+        first_contract_YYMM = get_YYMM_from_exchange_code(exchange_symbol_manager.get_exchange_symbol(current_date,generic_to_name[0])[-3:])  
+        current_idx = sorted_labels.index(first_contract_YYMM)
         _is_last_trading_day = 0
         _last_trading_date = exchange_symbol_manager.get_last_trading_date( current_date, generic_to_name[0] )
         if current_date == _last_trading_date:
