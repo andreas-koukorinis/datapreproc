17470f75b842ec3165ec349a488b1a2be4552b78
Sanchit Gupta <sanchit.gupta@tworoads.co.in>
2014-11-17 11:50:01 +0000

    removed warnings

--- DailyIndicators/StdDev.py
+++ DailyIndicators/StdDev.py
@@ -34,9 +34,12 @@ class StdDev( IndicatorListener ):
         daily_log_returns = array( [ item[1] for item in daily_log_returns_dt ] ).astype( float )
         n = daily_log_returns.shape[0]
         n = n-1 # To avoid using today's prices in calculation of indicators
-        _start_index = max( 0, n - self.period )  # If sufficient lookback not available,use the available data only to compute indicator
-        val = std( daily_log_returns[ _start_index : n ] )
-        if n < 2 or val == 0 :
+        _start_index = max( 0, n - int(self.period) ) # If sufficient lookback not available,use the available data only to compute indicator
+        if n > _start_index:
+            val = std( daily_log_returns[ _start_index : n ] )
+        else:
+            val = 0
+        if val == 0 :
             val=0.001  # Dummy value for insufficient lookback period(case where only 1 log return)
         self.values = ( daily_log_returns_dt[-1][0], val )
         for listener in self.listeners: 
