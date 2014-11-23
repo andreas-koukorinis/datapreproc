e30a83582715c3fb7b697a3ddbb3622ff153da53
Sanchit Gupta <sanchit.gupta@tworoads.co.in>
2014-11-23 20:27:55 +0000

    Now indicators are printed at the end of the simulation.Because otherwise last day may be skipped.Ran pylint on both GenData.py and Utils/PrintIndicators.py

--- GenData.py
+++ GenData.py
@@ -1,57 +1,55 @@
+'''Script to generate indicator values for research framework'''
 #!/usr/bin/env python
 import os
 import sys
-import datetime
-from importlib import import_module
 import ConfigParser
 from Dispatcher.Dispatcher import Dispatcher
 from Utils.PrintIndicators import PrintIndicators
 from Utils.Regular import get_all_products
 
-def __main__() :
+def __main__():
     # Command to run : python -W ignore GenData.py config_file <startdate enddate> <output_file>
     # Examples :
     # ./GenData.py test/config_PrintIndicators.cfg 2010-01-01 2013-12-31 ~/Downloads/temp.csv
     # ./GenData.py test/config_PrintIndicators.cfg # would take arguments from config file and create file in Data
 
-    if len ( sys.argv ) < 2 :
+    if len(sys.argv) < 2:
         print "config_file <trading-startdate trading-enddate> <indicator_file>"
         sys.exit(0)
     # Get handle of config file
     _config_file = sys.argv[1]
     _config = ConfigParser.ConfigParser()
-    _config.readfp( open( _config_file, 'r' ) )
+    _config.readfp(open(_config_file, 'r'))
 
     _directory = 'Data/'
     if not os.path.exists(_directory):
         os.makedirs(_directory)
-    if ( len ( sys.argv ) >= 4 ) :
+    if len(sys.argv) >= 4:
         _startdate = sys.argv[2]
         _enddate = sys.argv[3]
-    else :
-        _startdate=_config.get( 'Dates', 'start_date' )
-        _enddate=_config.get( 'Dates', 'end_date' )
+    else:
+        _startdate = _config.get('Dates', 'start_date')
+        _enddate = _config.get('Dates', 'end_date')
 
-    if ( len ( sys.argv ) >= 5 ) :
+    if len(sys.argv) >= 5:
         _indicator_file = sys.argv[4]
-    else :
-        _indicator_file = _directory+'print_indicators_'+os.path.splitext(_config_file)[0].split('/')[-1]+'.csv'
+    else:
+        _indicator_file = _directory+'indicators_'+os.path.splitext(_config_file)[0].split('/')[-1]+'.csv'
 
-    # Read product list from config file
-    _trade_products = _config.get( 'Products', 'trade_products' ).strip().split(",")
+    _all_products = get_all_products(_config)
 
-    _all_products = get_all_products( _config )
-
-
-    PrintIndicators.get_unique_instance( _all_products, _startdate, _enddate, _indicator_file, _config )
+    _print_indicators_instance = PrintIndicators.get_unique_instance(_startdate, _enddate, _indicator_file, _config)
 
     # Initialize Dispatcher using products list
-    _dispatcher = Dispatcher.get_unique_instance( _all_products, _startdate, _enddate, _config )
+    _dispatcher = Dispatcher.get_unique_instance(_all_products, _startdate, _enddate, _config)
 
-    # Run the dispatcher to start the backtesting process
+    # Run the dispatcher to start the simulation
     _dispatcher.run()
 
     # Effective number of trading days will be less than [end_date-start_date] due to the warmup time specified by the user
-    print '\nTotal Trading Days = %d'%( _dispatcher.trading_days )
+    print '\nTotal Trading Days = %d'%(_dispatcher.trading_days)
 
-__main__();
+    # Print the indicators
+    _print_indicators_instance.print_all_indicators()
+
+__main__()
--- Utils/PrintIndicators.py
+++ Utils/PrintIndicators.py
@@ -1,83 +1,69 @@
-import sys
-import pickle
+'''Module to print the indicator values for all the indicators
+specified in the config file
+The module is used by GenData.py for research framework'''
 import os
 from importlib import import_module
-
 from Utils.Regular import get_dt_from_date
 from DailyIndicators.Indicator_Listeners import IndicatorListener
 
 class PrintIndicators(IndicatorListener):
 
-    instance=[]
+    instance = []
 
-    def __init__(self, products, _startdate, _enddate, _indicator_file, _config):
-        self.directory = 'Data/'
-        self.indicators_file = _indicator_file
+    def __init__(self, _startdate, _enddate, _indicators_file, _config):
         self.start_date = get_dt_from_date(_startdate).date()
         self.end_date = get_dt_from_date(_enddate).date()
-        self.date = self.start_date
-        self.products=products
-        self.indicator_values={}
-        self.update_flag = {}
-        if not os.path.exists(self.directory):
-            os.makedirs(self.directory)
-
+        self.indicators_file = _indicators_file
+        self.latest_indicator_values = {}
+        self.all_indicator_values = {}
         # Read indicator list from config file
-        indicators = _config.get( 'DailyIndicators', 'names' ).strip().split(" ")
-        self.identifiers = sorted(indicators)
-
+        self.identifiers = sorted(_config.get('DailyIndicators', 'names').strip().split(" "))
         #Instantiate daily indicator objects
-        for indicator in indicators:
-            self.update_flag[indicator] = False
-            indicator_name = indicator.strip().split('.')[0]
-            module = import_module( 'DailyIndicators.' + indicator_name )
-            Indicatorclass = getattr( module, indicator_name )
-            indicator_instance = Indicatorclass.get_unique_instance( indicator, _startdate, _enddate, _config )
-            indicator_instance.add_listener(self)
-            self.indicator_values[indicator]=0 # Default value for each indicator
-
-        s='Date,' + ','.join(self.identifiers)
-        self.write_data(self.indicators_file,s,'w') # Write the header to the file
+        for identifier in self.identifiers:
+            indicator_name = identifier.strip().split('.')[0]
+            module = import_module('DailyIndicators.' + indicator_name)
+            _indicator_class = getattr(module, indicator_name)
+            _indicator_class.get_unique_instance(identifier, _startdate, _enddate, _config).add_listener(self)
+            self.latest_indicator_values[identifier] = 0.0 # Default value for each indicator
 
     @staticmethod
-    def get_unique_instance( products, _startdate, _enddate, _indicator_file, _config):
-        if(len(PrintIndicators.instance)==0):
-            new_instance = PrintIndicators(products, _startdate, _enddate, _indicator_file, _config )
+    def get_unique_instance(_startdate, _enddate, _indicators_file, _config):
+        if len(PrintIndicators.instance) == 0:
+            new_instance = PrintIndicators(_startdate, _enddate, _indicators_file, _config)
             PrintIndicators.instance.append(new_instance)
         return PrintIndicators.instance[0]
 
-    def print_indicators_readable_format(self):
-        s = str(self.date)
-        for identifier in self.identifiers:
-            if not self.update_flag[identifier] and identifier.split('.')[0]=='DailyLogReturns': # If DailyLogReturns indicaqtor is not updated,then use 0 as dummy value
-                s = s + ',0.0'
-            else: # For other indicators use previous value
-                s = s + ',' + str(self.indicator_values[identifier])
-        self.write_data(self.indicators_file,s,'a') # Write one line of indicator values for a particular date
-
-    #Save list of Date,IndicatorValues to a file in 'directory' for direct loading later
-    def print_indicators_pickle_format(indicator_instances,directory,start_dt):
-        for key in indicator_instances.keys():
-            filename = key.replace(',','-')
-            instance = indicator_instances[key]
-            values = [item for item in instance.values if item[0]>=start_dt.date()]
-            with open(directory+filename,'wb') as f:
-                pickle.dump(values,f)
-
-    def write_data(self,filename,data,mode):
-        f=open(self.indicators_file,mode)
-        f.write(data+'\n')
-        f.close()
-
-    def on_indicator_update(self,identifier,indicator_value):
-        if(type(indicator_value) is list):
-            indicator_value=indicator_value[-1] # Some indicators return full history of values as list,others return only the most recent value
-        current_date = indicator_value[0]
-        if(self.date < current_date):
-            self.print_indicators_readable_format()
-            #print_indicators_pickle_format()
-            self.date = current_date
+    def print_all_indicators(self):
+        '''After the Simulation has been done, this function
+           is called by GenData.py for printing the indicator
+           values to csv file'''
+        if not os.path.exists(os.path.dirname(self.indicators_file)):
+            os.makedirs(os.path.dirname(self.indicators_file))
+        _file = open(self.indicators_file, 'w')
+        _header = 'Date,' + ','.join(self.identifiers)
+        _out = _header + '\n'
+        _dates = sorted(self.all_indicator_values.keys())
+        for _date in _dates:
+            _line = [str(_date)]
             for _identifier in self.identifiers:
-                self.update_flag[_identifier] = False
-        self.update_flag[identifier] = True
-        self.indicator_values[identifier]=indicator_value[1]
+                _line.append(str(self.all_indicator_values[_date][_identifier]))
+            _out = _out + ','.join(_line) + '\n'
+        _file.write(_out)
+        _file.close()
+
+    def on_indicator_update(self, identifier, indicator_value):
+        '''When an indicator is updated,update its latest value
+           for printing later'''
+        if type(indicator_value) is list:
+            indicator_value = indicator_value[-1] # Some indicators return full history of values as list,others return only the most recent value
+        current_date = indicator_value[0]
+        self.latest_indicator_values[identifier] = indicator_value[1]
+        if current_date >= self.start_date and current_date <= self.end_date:
+            if current_date not in self.all_indicator_values.keys():
+                self.all_indicator_values[current_date] = {}
+                for _identifier in self.identifiers:
+                    if _identifier.split('.')[0] == 'DailyLogReturns': # For dailylogreturns indicator use 0.0 as the default value
+                        self.all_indicator_values[current_date][_identifier] = 0.0
+                    else: # For other indicators use the latest value as the default value
+                        self.all_indicator_values[current_date][_identifier] = self.latest_indicator_values[_identifier]
+            self.all_indicator_values[current_date][identifier] = indicator_value[1]
