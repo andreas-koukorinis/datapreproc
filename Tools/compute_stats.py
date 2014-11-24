323f07627a48c57189a3f48cdccbefb0f86c4589
Sanchit Gupta <sanchit.gupta@tworoads.co.in>
2014-11-24 07:42:15 +0000

    ~

--- Tools/compute_stats.py
+++ Tools/compute_stats.py
@@ -2,12 +2,10 @@ import pickle
 import sys
 from numpy import *
 import scipy.stats as ss
-import datetime
-import matplotlib.pyplot as plt
 
 def drawdown(returns):
     cum_returns = returns.cumsum()
-    return max(maximum.accumulate(cum_returns) - cum_returns)
+    return -1.0*(max(maximum.accumulate(cum_returns) - cum_returns))
 
 def rollsum(series,period):
     n = series.shape[0]
@@ -29,27 +27,10 @@ def mean_lowest_k_percent(series,k):
             _retval=mean(sorted_series[0:_index_of_worst_k_percent])
     return _retval
 
-
-def PlotPnLVersusDates(dates,log_returns,name):
-    num = int(len(dates)/5.0)
-    for i in xrange(0,len(dates)):
-        if(i%num!=0 and i!= len(dates)-1):
-            dates[i]=''
-        else:
-            dates[i] = dates[i].strftime('%d/%m/%Y')
-    cumulative_returns = (exp(log_returns.cumsum())-1)*100.0
-    plt.plot(cumulative_returns)
-    plt.xticks(range(len(cumulative_returns)),dates)
-    plt.xlabel('Date')
-    plt.ylabel('Cumulative Returns')
-    plt.savefig('Cumulative_Returns_'+name+".png", bbox_inches='tight')
-
-def analyse():
-    with open(sys.argv[1], 'rb') as f:
+def analyse(_returns_file):
+    with open(_returns_file, 'rb') as f:
         dates_returns = pickle.load(f)
-        daily_log_returns = [i[1] for i in dates_returns]
-        daily_log_returns = array(daily_log_returns).astype(float)
-        dates = [i[0] for i in dates_returns]
+        daily_log_returns = array([i[1] for i in dates_returns]).astype(float)
         net_returns = 100.0*(exp(sum(daily_log_returns))-1)
         monthly_log_returns = rollsum(daily_log_returns,21)
         quarterly_log_returns = rollsum(daily_log_returns,63)
@@ -67,10 +48,17 @@ def analyse():
         skewness = ss.skew(daily_log_returns)
         kurtosis = ss.kurtosis(daily_log_returns)
         max_dd_log = drawdown(daily_log_returns)
-        max_drawdown_percent = (exp(max_dd_log)-1)*100
+        max_drawdown_percent = abs((exp(max_dd_log)-1)*100)
         return_by_maxdrawdown = _annualized_returns_percent/max_drawdown_percent
-        PlotPnLVersusDates(dates,daily_log_returns,sys.argv[1].split('.')[0].split('_')[1])
+        print "\nNumber of Tradable Days = %d\n-------------RESULTS--------------------\nNet Returns = %.10f%%\nAnnualized_Returns = %.10f%% \nAnnualized_Std_Returns = %.10f%% \nSharpe Ratio = %.10f \nSkewness = %.10f\nKurtosis = %.10f\nDML = %.10f%%\nMML = %.10f%%\nQML = %.10f%%\nYML = %.10f%%\nMax Drawdown = %.10f%%\nReturn_drawdown_Ratio = %.10f \n" %(len(daily_log_returns),net_returns,_annualized_returns_percent,annualized_stddev_returns,sharpe,skewness,kurtosis,dml,mml,qml,yml,max_drawdown_percent,return_by_maxdrawdown)
 
-        print "\n-------------RESULTS--------------------\nNet Returns = %.10f%%\nAnnualized_Returns = %.10f%% \nAnnualized_Std_Returns = %.10f%% \nSharpe Ratio = %.10f \nSkewness = %.10f\nKurtosis = %.10f\nDML = %.10f%%\nMML = %.10f%%\nQML = %.10f%%\nYML = %.10f%%\nMax Drawdown = %.10f%%\nReturn_drawdown_Ratio = %.10f \n" %(net_returns,_annualized_returns_percent,annualized_stddev_returns,sharpe,skewness,kurtosis,dml,mml,qml,yml,max_drawdown_percent,return_by_maxdrawdown)
+def main():
+    if len( sys.argv ) > 1:
+        for i in range(1,len(sys.argv)):
+            print '\n\nStats for %s'%sys.argv[i]
+            analyse(sys.argv[i])
+    else:
+        sys.exit('python compute_stats.py return_file1 return_file2 .. .. returns_filen')
 
-analyse()
+if __name__ == '__main__':
+    main();
