411780c6aca05c1f65f7b220432053a71ae6c02c
Gaurav Chakravorty <gchak@circulumvite.com>
2014-11-27 15:16:19 -0500

    changes here are mostly for pylint, and some syntax errors

--- DailyIndicators/CorrelationLogReturns.py
+++ DailyIndicators/CorrelationLogReturns.py
@@ -1,10 +1,10 @@
 import numpy as np
+from Utils.Regular import get_dt_from_date
 from Indicator_Listeners import IndicatorListener
 from DailyLogReturns import DailyLogReturns
-from Utils.Regular import get_dt_from_date
 from portfolio_utils import get_products_from_portfolio_string
 
-class CorrelationLogReturns( IndicatorListener ):
+class CorrelationLogReturns(IndicatorListener):
     """Compute the correlation of log returns of a number of product for the specified number of days
 
     Attributes:
@@ -26,7 +26,7 @@ class CorrelationLogReturns( IndicatorListener ):
         self.date = get_dt_from_date(_startdate).date() # current date. whenever we get an update for a daet which is later than this date, we add a new row to self.logret_matrix
 
         # create a diagonal matrix
-        self.correlation_matrix = zeros ( len(self.products), len(self.products) )
+        self.correlation_matrix = np.zeros(len(self.products), len(self.products))
         for i in xrange(len(self.products)):
             self.correlation_matrix[i,i] = 1.0
 
@@ -41,33 +41,40 @@ class CorrelationLogReturns( IndicatorListener ):
 
     @staticmethod
     def get_unique_instance(identifier, _startdate, _enddate, _config):
-        # TODO ... this should be agnostic of order of the products in the portfolio string
-        _identifier = identifier.split('.')
-        _products = sorted(get_products_from_portfolio_string(_identifier[1]))
-        _sorted_identifier = _identifier[0] + '.' + make_portfolio_string_from_products(_products) + '.' + _identifier[2]
+        """Get the instance of the same indicator if already created before this.
+
+        This should be agnostic of order of the products in the portfolio string
+        """
+        _split_identifier_words = identifier.split('.')
+        _products = sorted(get_products_from_portfolio_string(_split_identifier_words[1]))
+        _sorted_identifier = _split_identifier_words[0] + '.' + make_portfolio_string_from_products(_products) + '.' + _split_identifier_words[2]
         if _sorted_identifier not in CorrelationLogReturns.instances.keys() :
-            new_instance = CorrelationLogReturns (_sorted_identifier, _startdate, _enddate, _config)
+            new_instance = CorrelationLogReturns(_sorted_identifier, _startdate, _enddate, _config)
             CorrelationLogReturns.instances[_sorted_identifier] = new_instance
         return CorrelationLogReturns.instances[_sorted_identifier]
 
-    # Update the standard deviation indicators on each ENDOFDAY event
     def on_indicator_update(self, identifier, daily_log_returns_dt):
+        """Update the standard deviation indicators on each ENDOFDAY event
+        """
         current_date = daily_log_returns_dt[-1][0]
         if current_date > self.date:
-            # this is the first update opf a new day
+            # If we are inside this if-statement, then this is the first update of a new day
             self.logret_matrix = np.insert(self.logret_matrix, self.logret_matrix.shape[0], [0.0]*len(self.products), axis=0)
             self.date = current_date
         self.logret_matrix[-1,self.map_identifier_to_index[identifier]] = daily_log_returns_dt[-1][1]
         if self.logret_matrix.shape[0] > self.logret_history:
-            self.logret_matrix = np.delete(self.logret_matrix, (0), axis=0)
+            self.logret_matrix = np.delete(self.logret_matrix,(0), axis=0)
         self.correlation_matrix_already_computed = False # this just marks that we need to recompute elements
 
-    # For performance reasons, perhaps we don't want to recompute this in on_indicator_update
-    def recompute ():
+
+    def get_correlation_matrix():
+        """Returns the correlation matrix of log returns
+        For performance reasons, perhaps we don't want to recompute this in on_indicator_update, but only when requested
+        """
         if not self.correlation_matrix_already_computed:
             self.covariance_matrix = np.cov(self.logret_matrix.T)
             # TODO, probably this can be made more efficient, since we already have the covariance matrix
             self.correlation_matrix = np.corrcoef(self.logret_matrix.T) 
             self.stddev_logret = np.std(self.logret_matrix,axis=0,ddof=1)
             self.correlation_matrix_already_computed = True
-        return (self.correlation_matrix)
+        return(self.correlation_matrix)
--- Strategies/target_risk_equal_risk_contribution.py
+++ Strategies/target_risk_equal_risk_contribution.py
@@ -19,61 +19,67 @@ class TargetRiskEqualRiskContribution(TradeAlgorithm):
     correlation_computation_interval :
 
     """
-    def init( self, _config ):
-        self.day=-1 # TODO move this to "watch" or a global time manager
-        self.target_risk = _config.getfloat( 'Strategy', 'target_risk' ) # this is the risk value we want to have. For now we are just interpreting that as the desired ex-ante stdev value. In future we will improve this to a better risk measure
-        self.rebalance_frequency = _config.getint( 'Parameters', 'rebalance_frequency' )
-        self.stddev_computation_history = _config.getint( 'Strategy', 'stddev_computation_history' )
-        self.stddev_computation_interval = _config.getint( 'Strategy', 'stddev_computation_interval' )
-        self.stddev_computation_indicator = _config.get( 'Strategy', 'stddev_computation_indicator' )
-        self.correlation_computation_history = _config.getint( 'Strategy', 'correlation_computation_history' )
-        self.correlation_computation_interval = _config.getint( 'Strategy', 'correlation_computation_interval' )
+    def init(self, _config):
+        self.day = -1 # TODO move this to "watch" or a global time manager
+        self.target_risk = _config.getfloat('Strategy', 'target_risk') # this is the risk value we want to have. For now we are just interpreting that as the desired ex-ante stdev value. In future we will improve this to a better risk measure
+        self.rebalance_frequency = _config.getint('Parameters', 'rebalance_frequency')
+        self.stddev_computation_history = np.max(2, _config.getint('Strategy', 'stddev_computation_history'))
+        self.stddev_computation_interval = np.max(1, _config.getint('Strategy', 'stddev_computation_interval'))
+        self.stddev_computation_indicator = _config.get('Strategy', 'stddev_computation_indicator')
+        self.correlation_computation_history = np.max(2, _config.getint('Strategy', 'correlation_computation_history'))
+        self.correlation_computation_interval = np.max(1, _config.getint('Strategy', 'correlation_computation_interval'))
         # Some computational variables
         self.last_date_correlation_computed = 0 # TODO change these to actual dates
         self.last_date_stdev_computed = 0 # TODO change thse to actual dates
         self.stdev_computation_indicator_mapping = {} # map from product to the indicator to get the stddev value
-        self.map_product_to_weight=dict([(product,0.0) for product in self.products]) # map from product to weight, which will be passed downstream
-        self.erc_weights = np.array([[0.0]*len(self.products)]) # these are the weights, with products occuring in the same order as the order in self.products
-        self.stddev_logret = np.array([[1.0]*len(self.products)]) # these are the stddev values, with products occuring in the same order as the order in self.products
+        self.map_product_to_weight = dict([(product, 0.0) for product in self.products]) # map from product to weight, which will be passed downstream
+        self.erc_weights = np.array([0.0]*len(self.products)) # these are the weights, with products occuring in the same order as the order in self.products
+        self.stddev_logret = np.array([1.0]*len(self.products)) # these are the stddev values, with products occuring in the same order as the order in self.products
+        # create a diagonal matrix of 1s for correlation matrix
+        self.logret_correlation_matrix = np.zeros(shape=(len(self.products), len(self.products)))
+        for i in xrange(len(self.products)):
+            self.correlation_matrix[i,i] = 1.0
 
-        self.map_product_to_index={} # this might be needed, dunno for sure
+        
+        self.map_product_to_index = {} # this might be needed, dunno for sure
         _product_index = 0
         for _product in self.products:
             self.map_product_to_index[_product] = _product_index
             _product_index = _product_index + 1
 
-        if ( is_valid_daily_indicator ( self.stddev_computation_indicator ) ):
+        if is_valid_daily_indicator(self.stddev_computation_indicator):
             for product in self.products:
-                _orig_indicator_name = self.stddev_computation_indicator + product + str(self.stddev_computation_history) #this would be something like StdDev.fTY.21
-                module = import_module( 'DailyIndicators.' + stddev_computation_indicator )
-                Indicatorclass = getattr( module, stddev_computation_indicator )
-                self.daily_indicators[_orig_indicator_name] = Indicatorclass.get_unique_instance( _orig_indicator_name, self.start_date, self.end_date, _config )
+                _orig_indicator_name = self.stddev_computation_indicator + product + str(self.stddev_computation_history) #this would be something like StdDev.fTY.252
+                module = import_module('DailyIndicators.' + stddev_computation_indicator)
+                Indicatorclass = getattr(module, stddev_computation_indicator)
+                self.daily_indicators[_orig_indicator_name] = Indicatorclass.get_unique_instance(_orig_indicator_name, self.start_date, self.end_date, _config)
                 self.stddev_computation_indicator[product] = self.daily_indicators[_orig_indicator_name]
                 # No need to attach ourselves as a listenr to the indicator for now. We are going to access the value directly
         else:
-            print ("Stdev computation indicator %s invalid!" %(self.stddev_computation_indicator))
+            print("Stdev computation indicator %s invalid!" %(self.stddev_computation_indicator))
             sys.exit(0)
 
-        _portfolio_string = make_portfolio_string_from_products ( self.products )
-        self.correlation_computation_indicator = CorrelationLogReturns.get_unique_instance( "CorrelationLogReturns" + '.' + _portfolio_string + '.' + str(self.correlation_computation_history), self.start_date, self.end_date, _config )
+        _portfolio_string = make_portfolio_string_from_products(self.products) # this allows us to pass a portfolio to the CorrelationLogReturns indicator.
+        # TODO Should we change the design of passing arguments to the indicators from a '.' concatenated list to a variable argument set?
+        self.correlation_computation_indicator = CorrelationLogReturns.get_unique_instance("CorrelationLogReturns" + '.' + _portfolio_string + '.' + str(self.correlation_computation_history), self.start_date, self.end_date, _config)
 
 
-    def on_events_update(self,events):
+    def on_events_update(self, events):
         all_eod = check_eod(events)  # Check whether all the events are ENDOFDAY
         if all_eod: self.day += 1  # Track the current day number
 
         # If today is the rebalancing day, then use indicators to calculate new positions to take
-        if all_eod and (self.day % self.rebalance_frequency == 0):
+        if all_eod and(self.day % self.rebalance_frequency == 0):
             _need_to_recompute_erc_weights = False # By default we don't need to change weights unless some input has changed
-            if self.day >= ( self.last_date_correlation_matrix_computed + self.correlation_computation_interval ):
+            if self.day >= (self.last_date_correlation_matrix_computed + self.correlation_computation_interval):
                 # we need to recompute the correlation matrix
                 self.logret_correlation_matrix = self.correlation_computation_indicator.recompute() # this command will not do anything if the values have been already computed. else it will
-                # TODO Add tests here for the correlation matrix to make sense, and otherwise do not overwrite previous value
-                # Or throw an erorr
+                # TODO Add tests here for the correlation matrix to make sense.
+                # If it fails, do not overwrite previous values, or throw an error
                 _need_to_recompute_erc_weights = True
                 self.last_date_correlation_matrix_computed = self.day
 
-            if self.day >= ( self.last_date_stdev_computed + self.stddev_computation_interval ):
+            if self.day >= (self.last_date_stdev_computed + self.stddev_computation_interval):
                 # Get the stdev values from the stddev indicators
                 for _product in self.products:
                     self.stddev_logret[self.map_product_to_index[_product]] = self.stddev_computation_indicator[_product].values[1] # TODO should not accessing an array without checking the length!
@@ -87,26 +93,27 @@ class TargetRiskEqualRiskContribution(TradeAlgorithm):
                 # compute covariance matrix from correlation matrix and
                 _cov_mat = self.logret_correlation_matrix * np.outer(self.stddev_logret, self.stddev_logret) # we should probably do it when either self.stddev_logret or _correlation_matrix has been updated
 
-                if np.sum ( self.erc_weights ) == 0:
+                if np.sum(self.erc_weights) == 0:
                     # Initialize weights
                     _annualized_risk = 100.0*(np.exp(np.sqrt(252.0)*self.stddev_logret)-1) # we should do this only when self.stddev_logret has been updated
                     zero_corr_risk_parity_weights = 1.0/(_annualized_risk)
-                    zero_corr_risk_parity_weights = zero_corr_risk_parity_weights/np.sum(np.abs(zero_corr_risk_parity_weights))
+                    self.erc_weights = zero_corr_risk_parity_weights/np.sum(np.abs(zero_corr_risk_parity_weights))
 
-                # Function to return the L1 norm of the series of { risk_contrib - man ( risk_contrib ) }, or sum of absolute values of the series
                 def _get_l1_norm_risk_contributions(_given_weights):
-                    _cov_vec = array(asmatrix( _cov_mat )*asmatrix( _given_weights ).T)[:,0]
+                    """Function to return the L1 norm of the series of { risk_contrib - man(risk_contrib) }, or sum of absolute values of the series
+                    """
+                    _cov_vec = np.array(np.asmatrix(_cov_mat)*np.asmatrix(_given_weights).T)[:, 0]
                     _trc = _given_weights*_cov_vec
-                    return sum(abs(_trc - mean(_trc)))
+                    return (np.sum(np.abs(_trc - np.mean(_trc))))
 
-                _constraints =  {'type':'eq', 'fun': lambda x: sum(abs(x)) - 1}
-                self.erc_weights = minimize ( _get_l1_norm_risk_contributions, self.erc_weights, method='SLSQP',constraints=_constraints,options={'ftol': 0.0000000000000000000000000001,'disp': True,'maxiter':10000 } ).x
+                _constraints = {'type':'eq', 'fun': lambda x: np.sum(np.abs(x)) - 1}
+                self.erc_weights = minimize(_get_l1_norm_risk_contributions, self.erc_weights, method='SLSQP', constraints=_constraints, options={'ftol': 0.0000000000000000000000000001, 'disp': True, 'maxiter':10000}).x
 
-                _annualized_stddev_of_portfolio = 100.0*(np.exp(np.sqrt(252.0*(np.asmatrix(self.erc_weights)*np.asmatrix(_cov_mat)*np.asmatrix(erc_weights).T))[0,0])-1)
+                _annualized_stddev_of_portfolio = 100.0*(np.exp(np.sqrt(252.0*(np.asmatrix(self.erc_weights)*np.asmatrix(_cov_mat)*np.asmatrix(erc_weights).T))[0, 0])-1)
                 self.erc_weights = self.erc_weights*(self.target_risk/_annualized_stddev_of_portfolio)
                 for _product in self.products:
                     self.map_product_to_weight[_product] = self.erc_weights[self.map_product_to_index[_product]] # This is completely avoidable use of map_product_to_index. We could just start an index at 0 and keep incrementing it
 
-            self.update_positions( events[0]['dt'], self.map_product_to_weight )
+            self.update_positions(events[0]['dt'], self.map_product_to_weight)
         else:
-            self.rollover( events[0]['dt'] )
+            self.rollover(events[0]['dt'])
