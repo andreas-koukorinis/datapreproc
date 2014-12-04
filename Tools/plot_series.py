a49be27d367e34280901620bcfd8d10e234bf611
Sanchit Gupta <sanchit.gupta@tworoads.co.in>
2014-12-04 06:28:29 +0000

    For pngs

--- Tools/plot_series.py
+++ Tools/plot_series.py
@@ -26,22 +26,7 @@ def plot_returns(_returns_files):
     plt.xticks(rotation=35)
     plt.savefig(_out_file+'.png') #bbox_inches='tight'
 
-def plot_series_pickle(_files):
-    _out_file = 'logs/pngs/'
-    for _file in _files:
-        _out_file += _file.split('/')[-2] + '_' + _file.split('/')[-1].split('.')[0] + '_'
-        with open(_file, 'rb') as f:
-            _dates_series = pickle.load(f)
-            _dates_series = [ list(t) for t in zip(*_dates_series) ]
-            _dates, _series = np.array(_dates_series[0]), np.array(_dates_series[1]).astype(float)
-            pt = plt.plot( _dates, _series, label=_file.split('/')[-2] )
-    plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.13),fancybox=True, ncol=4,prop={'size':6})
-    plt.xlabel("Time")
-    plt.ylabel(_files[0].split('/')[-1].split('.')[0])
-    plt.xticks(rotation=35)
-    plt.savefig(_out_file+'.png') #bbox_inches='tight'
-
-def plot_series_csv(_files):
+def plot_series(_files):
     _out_file = 'logs/pngs/'
     dateparse = lambda x: datetime.datetime.strptime(x, '%Y-%m-%d').date()
     ax_defined = False
@@ -68,6 +53,40 @@ def plot_series_csv(_files):
     plt.xticks(rotation=35)
     plt.savefig(_out_file+'.png') #bbox_inches='tight'
 
+def plot_series_separately(_files):
+    _out_file = 'logs/pngs/'
+    dateparse = lambda x: datetime.datetime.strptime(x, '%Y-%m-%d').date()
+    for _file in _files:
+        df = pd.read_csv(_file,parse_dates =['date'],header=0,date_parser=dateparse)
+        for column in df.columns:
+            if column != 'date':
+                df1 = df[['date',column]]
+                df1 = df1.set_index('date')
+                df1.plot()
+                plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.13),fancybox=True,  ncol=4,prop={'size':6})
+                plt.xlabel("Time")
+                plt.ylabel(column)
+                plt.xticks(rotation=35)
+                plt.savefig('logs/pngs/'+column+'.png')
+
+def plot_returns_separately(_files):
+    _out_file = 'logs/pngs/'
+    dateparse = lambda x: datetime.datetime.strptime(x, '%Y-%m-%d').date()
+    for _file in _files:
+        df = pd.read_csv(_file,parse_dates =['date'],header=0,date_parser=dateparse)
+        for column in df.columns:
+            if column != 'date':
+                df1 = df[['date',column]]
+                df1 = df1.set_index('date')
+                _returns = df1[column].values
+                df1[column] = ( np.exp( np.cumsum( _returns ) ) - 1 )*100.0
+                df1.plot()
+                plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.13), fancybox=True, ncol=4, prop={'size':6})
+                plt.xlabel("Time")
+                plt.ylabel(column)
+                plt.xticks(rotation=35)
+                plt.savefig('logs/pngs/'+column+'.png')
+
 def main():
     if len( sys.argv ) > 1:
         _directory = 'logs/pngs/'
@@ -80,8 +99,13 @@ def main():
             _files.append(sys.argv[i])
         if _type == 0:
             plot_returns(_files)
-        else:
-            plot_series_csv(_files)
+        elif _type == 1:
+            plot_series(_files)
+        elif _type == 2:
+            plot_returns_separately(_files)
+        elif _type == 3:
+            plot_series_separately(_files)
+
     else:
         sys.exit('python plot_series.py type file1 file2 .. .. filen\nType 0 : returns\nType 1: other series')
 
