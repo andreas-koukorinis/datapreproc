cc83158444c6665d9e496d3c2f0867b072019bce
Sanchit Gupta <sanchit.gupta@tworoads.co.in>
2014-12-08 08:42:36 +0000

    To dump history of daily updates

---
+++ DataCleaning/csi_scripts/run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+for i in {36..1}; do echo $i;python daily_update.py futures $i EBL EBM FLG ZF TY AD1 BP1 CD1 CU1 MP FDX SXF NK EX JT ES SXE; done
+#for i in {36..1}; do echo $i;python daily_update.py futures $i ZN; done
+#python daily_update1.py futures 31 EBL EBM FLG ZF TY AD1 BP1 CD1 CU1 MP FDX SXF NK EX JT ES SXE
