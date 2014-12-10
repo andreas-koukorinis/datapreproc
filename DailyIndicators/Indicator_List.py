6d935eef10e6121c82eba9688a8c168b1356d38e
gchak_gchaklinux <gchak@circulumvite.com>
2014-12-10 12:40:03 -0500

    added support for module file name differetn than class name

--- DailyIndicators/Indicator_List.py
+++ DailyIndicators/Indicator_List.py
@@ -8,3 +8,25 @@ def is_valid_daily_indicator ( _daily_indicator_module_name ) :
         print ( "DailyIndicator module name %s not valid" %(_daily_indicator_module_name) )
     return (_retval)
 
+def get_module_name_from_indicator_name ( indicator_name ) :
+    if is_valid_daily_indicator ( indicator_name ):
+        if indicator_name == "AverageDiscretizedTrend":
+            return ("AverageDiscretizedTrend")
+        if indicator_name == "AverageStdDev":
+            return ("AverageStdDev")
+        if indicator_name == "CorrelationLogReturns":
+            return ("CorrelationLogReturns")
+        if indicator_name == "DailyLogReturns":
+            return ("DailyLogReturns")
+        if indicator_name == "MovingAverage":
+            return ("MovingAverage")
+        if indicator_name == "StdDevCrossover":
+            return ("StdDevCrossover")
+        if indicator_name == "StdDev":
+            return ("StdDev")
+        if indicator_name == "Trend":
+            return ("Trend")
+        return (indicator_name)
+    else:
+        print ( STDERR "get_module_name_from_indicator_name called with bad indicator_name %s" %(indicator_name) )
+        sys.exit(0)
