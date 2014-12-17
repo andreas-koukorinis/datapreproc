605c7de938a9e2c236bb623ffb2bf6db050fec2a
Sanchit Gupta <sanchit.gupta@tworoads.co.in>
2014-12-17 01:08:08 +0000

    signals

---
+++ signals/signal_list.py
@@ -0,0 +1,17 @@
+def is_valid_signal_name(_signal_name):
+    _signal_name_list = ['TargetRiskRP']
+    _retval = False
+    if _signal_name in _signal_name_list:
+        _retval = True
+    else :
+        print "Signalname %s not valid" % _signal_name
+    return _retval
+
+def get_module_name_from_signal_name(signal_name):
+    def _target_risk_rp():
+        return "target_risk_rp"
+
+    options = { 'TargetRiskRP' : _target_risk_rp
+              }
+    if is_valid_signal_name(signal_name):
+        return options[signal_name]()
