a8e1f15086bef57bfe87121af9870bd0a8efbb92
Gaurav Chakravorty <gchak@circulumvite.com>
2014-12-19 14:42:24 +0000

    Sanchit's new algorithm, to handle cases where weights of two strategies on a product cancel out

--- Strategies/aggregator_cwas.py
+++ Strategies/aggregator_cwas.py
@@ -56,27 +56,33 @@ class AggregatorCWAS(TradeAlgorithm):
     def update_past_relative_contribution(self, _new_signal_contributions, _new_portfolio_weights):
         for i in range(len(_new_signal_contributions)):
             for _product in self.products:
-                if _new_portfolio_weights[_product] == 0:
+                if _new_portfolio_abs_weights[_product] < 0.00000001:
                     self.past_relative_contribution[i][_product] = 0
+                elif(_new_portfolio_abs_weights[_product] > 0.0000001 and abs(_new_portfolio_weights[_product]) < 0.0000001):
+                    self.past_relative_contribution[i][_product] = _new_signal_contributions[i][_product]
                 else:
                     self.past_relative_contribution[i][_product] = _new_signal_contributions[i][_product]/_new_portfolio_weights[_product]
 
     def get_new_portfolio_weights(self, _signal_rebalancing_day, _current_portfolio_weights, _signals, _signal_allocations):
         _new_signal_contributions = []
         _new_portfolio_weights = dict([(_product, 0.0) for _product in self.products])
+        _new_portfolio_abs_weights = dict([(_product, 0.0) for _product in self.products])
         for i in range(len(_signals)):
             _new_signal_contributions.append(dict([(_product, 0.0) for _product in self.products]))
             if _signal_rebalancing_day[i]:
                 for _product in self.products:
-                    _new_signal_contributions[i][_product] = _signal_allocations[i] * _signals[i].weights.get(_product, 0.0) # .get with 0.0 makes sure that if the product is not there in the map then we return 0
+                    _new_signal_contributions[i][_product] = _signal_allocations[i] * _signals[i].weights.get(_product, 0.0)
                     _new_portfolio_weights[_product] += _new_signal_contributions[i][_product]
+                    _new_portfolio_abs_weights[_product] += abs(_new_signal_contributions[i][_product])
             else:
-                # if it isn't a rebalancing day, then 
                 for _product in self.products:
-                    _new_signal_contributions[i][_product] = _current_portfolio_weights[_product] * self.past_relative_contribution[i][_product]
+                    if abs(_current_portfolio_weights[_product]) < 0.0000001 and self.past_relative_contribution[i][_product] != 0: #shoudl change the != 0 here
+                        _new_signal_contributions[i][_product] = self.past_relative_contribution[i][_product]
+                    else:
+                        _new_signal_contributions[i][_product] = _current_portfolio_weights[_product] * self.past_relative_contribution[i][_product]
                     _new_portfolio_weights[_product] += _new_signal_contributions[i][_product]
-        #print self.signals[0].weights, self.signals[1].weights, _current_portfolio_weights, _new_portfolio_weights, self.past_relative_contribution, _new_signal_contributions
-        self.update_past_relative_contribution(_new_signal_contributions, _new_portfolio_weights) # TODO should not call here
+                    _new_portfolio_abs_weights[_product] += abs(_new_signal_contributions[i][_product])
+        self.update_past_relative_contribution(_new_signal_contributions, _new_portfolio_weights, _new_portfolio_abs_weights)
         return _new_portfolio_weights
          
     def on_events_update(self, events):
