16bf8c640472456048e79033abd290af41bb3605
Sanchit Gupta <sanchit.gupta@tworoads.co.in>
2014-12-22 10:31:22 +0000

    added flip fix,second contract based on first

--- DataCleaning/process_futures_oi_based.py
+++ DataCleaning/process_futures_oi_based.py
@@ -28,14 +28,6 @@ def compare_ticker(ticker1, ticker2):
     code2 = convert_to_year_month(ticker2[-2:] + month_codes_rev[ticker2[-3]])
     return code1 > code2
 
-def check(df1, df2):
-    for i in range(len(df1)-2):
-        if compare_ticker(df1.loc[i, 'specific_ticker'], df1.loc[i+1, 'specific_ticker']):
-            print 'error,%d,1'%i
-    for i in range(len(df2)-2):
-        if compare_ticker(df2.loc[i, 'specific_ticker'], df2.loc[i+1, 'specific_ticker']):
-            print 'error,%d,2'%i
-
 def flips_present(df):
     flips = []
     for i in range(len(df)-2):
@@ -48,11 +40,31 @@ def flips_present(df):
         print 'No flips found'
         return False
 
-def fix_flips(df):
-    pass
+def fix_flips(in_dir, df):
+    for i in range(len(df)-2):
+        if compare_ticker(df.loc[i, 'specific_ticker'], df.loc[i+1, 'specific_ticker']):
+            cmd = "cd %s;grep -H %s %s | sed 's/:/,/'"%(in_dir, df.loc[i+1,'date'], df.loc[i, 'contract_filename'])
+            output = commands.getoutput(cmd).split(',')
+            df.loc[i+1] = [output[0],output[1],output[2],output[3],output[4],output[5],output[6],output[7],output[8],output[9],df.loc[i, 'specific_ticker']]
+    #df = df[['contract_filename', 'date','open','high','low','close','contract_volume','contract_oi','total_volume','total_oi']]
+    return df
 
-def get_second_contract(in_dir, oi_1):
-    pass 
+def get_second_contract(in_dir, df,  out_file_aux1, out_file_aux2, oi_1):
+    cmd = "cat %s | awk -F, '{ print $1 }' | uniq"%(out_file_aux1)
+    contract_filenames = commands.getoutput(cmd).split('\n')
+    j = 1
+    f = open(out_file_aux2, 'w')
+    for i in range(len(df)-2):
+        if df.loc[i, 'contract_filename'] == contract_filenames[j]:
+            j += 1
+            if j == len(contract_filenames):
+                break
+        if j != len(contract_filenames): 
+            date =  df.loc[i, 'date']
+            cmd = "cd %s;grep -H %s %s | sed 's/:/,/'"%(in_dir, str(date), contract_filenames[j])
+            output = commands.getoutput(cmd)
+            f.write(output)
+    f.close()
 
 def process_futures(product, to_name):
     # Directory for the product
@@ -65,8 +77,8 @@ def process_futures(product, to_name):
     out_dir = 'Data/'
 
     # Remove 0000 file
-    _aux_file1 = in_directory + product + '_0000.csv'
-    _aux_file2 = in_directory + product + '__0000.csv'
+    _aux_file1 = in_dir + product + '_0000.csv'
+    _aux_file2 = in_dir + product + '__0000.csv'
     if os.path.exists(_aux_file1):
         os.remove(_aux_file1)
     if os.path.exists(_aux_file2):
@@ -74,31 +86,33 @@ def process_futures(product, to_name):
 
     # Get first contract data based on highest open interest
     out_file_aux1 = out_dir + 'aux1_' + product + '.csv'
-    cmd = "cd %s;for dd_ in `cat *csv | sort -g | awk -F, '{ print $1 }' | uniq`;do grep $dd_ *csv | sort -k7,7 -rg -t, | head -n1 | sed 's/:/,/' ;done"%(directory)
+    cmd = "cd %s;for dd_ in `cat *csv | sort -g | awk -F, '{ print $1 }' | uniq`;do grep $dd_ *csv | sort -k7,7 -rg -t, | head -n1 | sed 's/:/,/' ;done"%(in_dir)
     output = commands.getoutput(cmd)
     f = open(out_file_aux1, 'w')
     f.write(output)
     f.close()
 
     # Load the first contract data and check for flips 
-    dateparse = lambda x: datetime.strptime(x, '%Y%m%d').date() # Parse dates in format required by mysql i.e. YYYY-MM-DD
-    oi_1 = pd.read_csv(out_file_aux1, parse_dates =['date'], date_parser = dateparse, names=['contract_filename', 'date','open','high','low','close','contract_volume','contract_oi','total_volume','total_oi'])
+    oi_1 = pd.read_csv(out_file_aux1, names=['contract_filename', 'date','open','high','low','close','contract_volume','contract_oi','total_volume','total_oi'])
     oi_1['specific_ticker'] = oi_1.apply(lambda row: get_specific_ticker_from_filename(row['contract_filename'], product), axis=1)
     if flips_present(oi_1):
-        oi_1 = fix_flips(oi_1)
+        oi_1 = fix_flips(in_dir, oi_1)
+        flips_present(oi_1)
+        oi_1 = oi_1[['contract_filename', 'date','open','high','low','close','contract_volume','contract_oi','total_volume','total_oi']]
+        #flips_present(oi_1)
+        out_file_new_aux1 = out_dir + 'aux1_new_' + product + '.csv'
+        oi_1.to_csv(out_file_new_aux1, index=False, header=False)
 
     # Get the data for second contract based on first contract
     out_file_aux2 = out_dir + 'aux2_' + product + '.csv'
-    output = get_second_contract(in_dir, oi_1)
-    f = open(out_file_aux2, 'w')
-    f.write(output)
-    f.close()
+    get_second_contract(in_dir, oi_1, out_file_new_aux1, out_file_aux2, oi_1)
 
     # Process the data and output to db format
     generic_tickers = [product + '_1', product + '_2']
     output_path_1 = out_dir + generic_tickers[0] + '.csv'
     output_path_2 = out_dir + generic_tickers[1] + '.csv'   
-    oi_1 = pd.read_csv(out_file_aux1,parse_dates=['date'],date_parser = dateparse,names=['contract_filename', 'date','open','high','low','close','contract_volume','contract_oi','total_volume','total_oi'])
+    dateparse = lambda x: datetime.strptime(x, '%Y%m%d').date() # Parse dates in format required by mysql i.e. YYYY-MM-DD
+    oi_1 = pd.read_csv(out_file_new_aux1,parse_dates=['date'],date_parser = dateparse,names=['contract_filename', 'date','open','high','low','close','contract_volume','contract_oi','total_volume','total_oi'])
     oi_2 = pd.read_csv(out_file_aux2,parse_dates=['date'],date_parser = dateparse,names=['contract_filename', 'date','open','high','low','close','contract_volume','contract_oi','total_volume','total_oi'])
 
     oi_1['specific_ticker'] = oi_1.apply(lambda row: get_specific_ticker_from_filename(row['contract_filename'], to_name), axis=1)
@@ -119,7 +133,6 @@ def process_futures(product, to_name):
             oi_2.loc[i,'is_last_trading_day'] = 0.0
     oi_2 = oi_2[['date','product','specific_ticker','open','high','low','close','is_last_trading_day','contract_volume','contract_oi','total_volume','total_oi']]
 
-    check(oi_1, oi_2)
     oi_1.to_csv(output_path_1, index=False)
     oi_2.to_csv(output_path_2, index=False)
 
@@ -133,4 +146,4 @@ def __main__():
     process_futures(product, to_name)
 
 if __name__ == '__main__':
-    __main__();
+    __main__()
