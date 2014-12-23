0e052f43bb022b551509d8dcdd2b84f2e92060bd
Gaurav Chakravorty <gchak@circulumvite.com>
2014-12-23 13:44:20 -0500

    rp moved to volatility parity

--- signals/signal_list.py
+++ signals/signal_list.py
@@ -1,5 +1,5 @@
 def is_valid_signal_name(_signal_name):
-    _signal_name_list = ['TargetRiskRP', 'TargetRiskVolatilityParity', 'TargetRiskEqualRiskContribution', 'TargetRiskMaxSharpeHistCorr', 'UnleveredDMF']
+    _signal_name_list = ['TargetRiskVolatilityParity', 'TargetRiskEqualRiskContribution', 'TargetRiskMaxSharpeHistCorr', 'UnleveredDMF']
     _retval = False
     if _signal_name in _signal_name_list:
         _retval = True
@@ -8,8 +8,6 @@ def is_valid_signal_name(_signal_name):
     return _retval
 
 def get_module_name_from_signal_name(signal_name):
-    def _target_risk_rp():
-        return "target_risk_rp"
     def _target_risk_volatility_parity():
         return "target_risk_volatility_parity"
     def _target_risk_equal_risk_contribution():
@@ -19,8 +17,7 @@ def get_module_name_from_signal_name(signal_name):
     def _unlevered_dmf():
         return ( "unlevered_dmf" )
 
-    options = { 'TargetRiskRP' : _target_risk_rp,
-                'TargetRiskVolatilityParity' : _target_risk_volatility_parity,
+    options = { 'TargetRiskVolatilityParity' : _target_risk_volatility_parity,
                 'TargetRiskEqualRiskContribution' : _target_risk_equal_risk_contribution,
                 'TargetRiskMaxSharpeHistCorr' : _target_risk_max_sharpe_hist_corr,
                 'UnleveredDMF' : _unlevered_dmf
