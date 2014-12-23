2f39685b2ba6575d9039978554ddda622f4ec41b
Gaurav Chakravorty <gchak@circulumvite.com>
2014-12-23 16:24:04 +0000

    added sign correction logic

--- signals/target_risk_equal_risk_contribution.py
+++ signals/target_risk_equal_risk_contribution.py
@@ -4,6 +4,7 @@ from importlib import import_module
 from scipy.optimize import minimize
 from Algorithm.signal_algorithm import SignalAlgorithm
 from Utils.Regular import check_eod,parse_weights
+from Utils.correct_signs_weights import correct_signs_weights
 from DailyIndicators.Indicator_List import is_valid_daily_indicator
 from DailyIndicators.CorrelationLogReturns import CorrelationLogReturns
 from DailyIndicators.portfolio_utils import make_portfolio_string_from_products
@@ -146,11 +147,19 @@ class TargetRiskEqualRiskContribution(SignalAlgorithm):
 
                 _constraints = {'type':'eq', 'fun': lambda x: numpy.sum(numpy.abs(x)) - 1}
                 self.erc_weights_optim = minimize(_get_l1_norm_risk_contributions, self.erc_weights_optim, method='SLSQP', constraints=_constraints, options={'ftol': self.optimization_ftol, 'disp': False, 'maxiter':self.optimization_maxiter}).x
-                # TODO{gchak} We should check whether weights produced here have the same signs as self.allocation_signs.
-                # Otherwise we need to set them to 0.
-                # Perhaps it might be better to add a constraint in optimization that if sign(_given_weights) != sign(self.allocation_signs)
-                # then return a very high value indicating that it is not a direction we should go towards.
                 self.erc_weights = self.erc_weights_optim
+
+                # We check whether weights produced here have the same signs as self.allocation_signs.
+                # Otherwise we try to correct them
+                if sum(numpy.abs(numpy.sign(self.erc_weights)-numpy.sign(self.allocation_signs))) > 0:
+                    # some sign isn't what it shoudl be
+                    _check_sign_of_weights = False # this is sort of a debugging exercise
+                    if _check_sign_of_weights:
+                        print ( "Sign-check-fail: On date %s weights %s" %(events[0]['dt'], [ str(x) for x in self.erc_weights ]) )
+                    _annualized_risk = 100.0*(numpy.exp(numpy.sqrt(252.0)*self.stddev_logret)-1) # we should do this only when self.stddev_logret has been updated
+                    _expected_sharpe_ratios = self.allocation_signs # switched to self.allocation_signs from not multiplying anything 
+                    zero_corr_risk_parity_weights = (1.0/_annualized_risk) * _expected_sharpe_ratios
+                    self.erc_weights = correct_signs_weights(self.erc_weights, zero_corr_risk_parity_weights)
 
                 # In the following steps we resize the portfolio to the taregt risk level.
                 # We have just used stdev as the measure of risk ehre since it is simple.
@@ -158,11 +167,6 @@ class TargetRiskEqualRiskContribution(SignalAlgorithm):
                 _annualized_stddev_of_portfolio = 100.0*(numpy.exp(numpy.sqrt(252.0 * (numpy.asmatrix(self.erc_weights) * numpy.asmatrix(_cov_mat) * numpy.asmatrix(self.erc_weights).T))[0, 0]) - 1)
                 self.erc_weights = self.erc_weights*(self.target_risk/_annualized_stddev_of_portfolio)
 
-                #TODO figure out signs compatibility with mandate
-                _check_sign_of_weights = False
-                if _check_sign_of_weights:
-                    if sum(numpy.abs(numpy.sign(self.erc_weights)-numpy.sign(self.allocation_signs))) > 0 :
-                        print ( "Sign-check-fail: On date %s weights %s" %(events[0]['dt'], [ str(x) for x in self.erc_weights ]) )
                 
                 for _product in self.products:
                     self.map_product_to_weight[_product] = self.erc_weights[self.map_product_to_index[_product]] # This is completely avoidable use of map_product_to_index. We could just start an index at 0 and keep incrementing it
