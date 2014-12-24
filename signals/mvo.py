3ae7e005eaab4b3468c06ca47761b7878f32b365
debidatta <debi.me@gmail.com>
2014-12-24 19:24:28 +0530

    some pylint issues resolved

--- signals/mvo.py
+++ signals/mvo.py
@@ -1,21 +1,21 @@
 import sys
 from importlib import import_module
-from math import sqrt, exp
 import numpy
 from numpy import hstack, vstack
 from cvxopt import matrix, solvers
 from cvxopt.solvers import qp
 from Algorithm.signal_algorithm import SignalAlgorithm
-from Utils.Regular import check_eod, parse_weights
+from Utils.Regular import check_eod
 from DailyIndicators.Indicator_List import is_valid_daily_indicator
 from DailyIndicators.portfolio_utils import make_portfolio_string_from_products
 from DailyIndicators.CorrelationLogReturns import CorrelationLogReturns
+
 
 class MVO(SignalAlgorithm):
     """Perform mean variance optimization"""
 
     # Helper functions to succinctly represent constraints for optimizer
-    def lag(self,x, k):
+    def lag(self, x, k):
         """ Helper function lag(x,k)
         Produces a lag of k in a series x with leading zeros
         Args:
@@ -34,7 +34,7 @@ class MVO(SignalAlgorithm):
         else:
             return k*[0]+x[0:-k]
 
-    def shift_vec(self,x, m):
+    def shift_vec(self, x, m):
         """ Helper function shift_vec(x,m)
         Produces a matrix of vectors each with a lag i where i ranges from 0 to m-1
         Args:
@@ -51,17 +51,7 @@ class MVO(SignalAlgorithm):
         mat = matrix(mat, (n, m))
         return mat
 
-            # Function that calculates the efficient frontier  
-    # by minimizing (Variance - tolerance * expected returns)
-    # with the given constraints
-    # sum(abs(weights)) <= 5
-    # Input: returns - matrix containing log daily returns for n securities.
-    #        tolerance_limit - upper bound of tolerance in function to be minimized
-    #        max_allocation - maximum weight to be allocated to one security
-    #                         (set low threshold to diversify)
-    # Output: frontier - matrix of optimal weight for each value of tolerance with 
-    #                    performance stats exp.returns, std.dev, sharpe ratio
-    def efficient_frontier(self,expected_returns, covariance, leverage, risk_tolerance, max_allocation=0.5):
+    def efficient_frontier(self, expected_returns, covariance, leverage, risk_tolerance, max_allocation=0.5):
         """ Function that calculates the efficient frontier
             by minimizing (Variance - risk tolerance * expected returns)
             with a given lerverage and risk tolance
@@ -168,11 +158,11 @@ class MVO(SignalAlgorithm):
         self.last_date_correlation_matrix_computed = 0
         self.last_date_stdev_computed = 0
         self.last_date_exp_return_computed = 0
-        self.stdev_computation_indicator_mapping = {} # map from product to the indicator to get the stddev value
+        self.stdev_computation_indicator_mapping = {}  # map from product to the indicator to get the stddev value
         self.exp_log_returns = numpy.array([0.0]*len(self.products))
-        self.map_product_to_weight = dict([(product, 0.0) for product in self.products]) # map from product to weight, which will be passed downstream
-        self.weights = numpy.array([0.0]*len(self.products)) # these are the weights, with products occuring in the same order as the order in self.products
-        self.stddev_logret = numpy.array([1.0]*len(self.products)) # these are the stddev values, with products occuring in the same order as the order in self.products
+        self.map_product_to_weight = dict([(product, 0.0) for product in self.products])  # map from product to weight, which will be passed downstream
+        self.weights = numpy.array([0.0]*len(self.products))  # these are the weights, with products occuring in the same order as the order in self.products
+        self.stddev_logret = numpy.array([1.0]*len(self.products))  # these are the stddev values, with products occuring in the same order as the order in self.products
         # Create a diagonal matrix of 1s for correlation matrix
         self.logret_correlation_matrix = numpy.eye(len(self.products))
         # Set indicator for ExpectedReturns
@@ -186,18 +176,17 @@ class MVO(SignalAlgorithm):
         # Set indicator for stddev_computation_indicator
         if is_valid_daily_indicator(self.stddev_computation_indicator):
             for product in self.products:
-                _orig_indicator_name = self.stddev_computation_indicator + '.' + product + '.' + str(self.stddev_computation_history) # this would be something like StdDev.fTY.252
+                _orig_indicator_name = self.stddev_computation_indicator + '.' + product + '.' + str(self.stddev_computation_history)  # this would be something like StdDev.fTY.252
                 module = import_module('DailyIndicators.' + self.stddev_computation_indicator)
                 Indicatorclass = getattr(module, self.stddev_computation_indicator)
                 self.daily_indicators[_orig_indicator_name] = Indicatorclass.get_unique_instance(_orig_indicator_name, self.start_date, self.end_date, _config)
         else:
-            print("Stdev computation indicator %s invalid!" %(self.stddev_computation_indicator))
+            print "Stdev computation indicator %s invalid!" % self.stddev_computation_indicator
             sys.exit(0)
         # Set correlation computation indicator
-        _portfolio_string = make_portfolio_string_from_products(self.products) # this allows us to pass a portfolio to the CorrelationLogReturns indicator.
+        _portfolio_string = make_portfolio_string_from_products(self.products)  # this allows us to pass a portfolio to the CorrelationLogReturns indicator.
         # TODO Should we change the design of passing arguments to the indicators from a '.' concatenated list to a variable argument set?
         self.correlation_computation_indicator = CorrelationLogReturns.get_unique_instance("CorrelationLogReturns" + '.' + _portfolio_string + '.' + str(self.correlation_computation_history), self.start_date, self.end_date, _config)
-
 
     def on_events_update(self, events):
         """Implement strategy and update weights"""
@@ -207,10 +196,10 @@ class MVO(SignalAlgorithm):
 
         # If today is the rebalancing day, then use indicators to calculate new positions to take
         if all_eod and(self.day % self.rebalance_frequency == 0):
-            _need_to_recompute_weights = False # By default we don't need to change weights unless some input has changed
+            _need_to_recompute_weights = False  # By default we don't need to change weights unless some input has changed
             if self.day >= (self.last_date_correlation_matrix_computed + self.correlation_computation_interval):
                 # we need to recompute the correlation matrix
-                self.logret_correlation_matrix = self.correlation_computation_indicator.get_correlation_matrix() # this command will not do anything if the values have been already computed. else it will
+                self.logret_correlation_matrix = self.correlation_computation_indicator.get_correlation_matrix()  # this command will not do anything if the values have been already computed. else it will
                 # TODO Add tests here for the correlation matrix to make sense.
                 # If it fails, do not overwrite previous values, or throw an error
                 _need_to_recompute_weights = True
@@ -219,7 +208,7 @@ class MVO(SignalAlgorithm):
             if self.day >= (self.last_date_stdev_computed + self.stddev_computation_interval):
                 # Get the stdev values from the stddev indicators
                 for _product in self.products:
-                    self.stddev_logret[self.map_product_to_index[_product]] = self.daily_indicators[self.stddev_computation_indicator + '.' + _product + '.' + str(self.stddev_computation_history)].values[1] # earlier this was self.stddev_computation_indicator[_product] but due to error in line 57, switched to this
+                    self.stddev_logret[self.map_product_to_index[_product]] = self.daily_indicators[self.stddev_computation_indicator + '.' + _product + '.' + str(self.stddev_computation_history)].values[1]  # earlier this was self.stddev_computation_indicator[_product] but due to error in line 57, switched to this
                     # TODO should not accessing an array without checking the length!
                     # TODO should add some sanity checks before overwriting previous value.
                     # TODO we can make tests here that the module needs to pass.
@@ -227,23 +216,23 @@ class MVO(SignalAlgorithm):
                 self.last_date_stdev_computed = self.day
 
             if self.day >= (self.last_date_exp_return_computed + self.exp_return_computation_interval):
-                # Get the expected log return values values from the expected returns indicators            
+                # Get the expected log return values values from the expected returns indicators
                 for _product in self.products:
                     self.exp_log_returns[self.map_product_to_index[_product]] = self.daily_indicators[self.expreturns_indicator + '.' + _product + '.' + str(self.exp_return_computation_history)].values[1]
                 _need_to_recompute_weights = True
                 self.last_date_exp_return_computed = self.day
-            
-            if _need_to_recompute_weights :
+
+            if _need_to_recompute_weights:
                 # Calculate weights to assign to each product using indicators
                 # compute covariance matrix from correlation matrix and
-                _cov_mat = self.logret_correlation_matrix * numpy.outer(self.stddev_logret, self.stddev_logret) # we should probably do it when either self.stddev_logret or _correlation_matrix has been updated
+                _cov_mat = self.logret_correlation_matrix * numpy.outer(self.stddev_logret, self.stddev_logret)  # we should probably do it when either self.stddev_logret or _correlation_matrix has been updated
                 # Recompute weights
-                
-                self.weights = self.efficient_frontier(self.exp_log_returns, _cov_mat, self.leverage, self.risk_tolerance, self.max_allocation)
-                
-            for _product in self.products:
-                    self.map_product_to_weight[_product] = self.weights[self.map_product_to_index[_product]] # This is completely avoidable use of map_product_to_index. We could just start an index at 0 and keep incrementing it
 
-            self.update_positions(events[0]['dt'], self.map_product_to_weight)    
+                self.weights = self.efficient_frontier(self.exp_log_returns, _cov_mat, self.leverage, self.risk_tolerance, self.max_allocation)
+
+            for _product in self.products:
+                    self.map_product_to_weight[_product] = self.weights[self.map_product_to_index[_product]]  # This is completely avoidable use of map_product_to_index. We could just start an index at 0 and keep incrementing it
+
+            self.update_positions(events[0]['dt'], self.map_product_to_weight)
         else:
             self.rollover(events[0]['dt'])
