bf9c419696cb0d9710cca35d616d3f776e66a302
debidatta <debi.me@gmail.com>
2014-12-30 18:31:36 +0530

    Added plot option with labels and line to show when backfilling starts

--- DataCleaning/backfill.py
+++ DataCleaning/backfill.py
@@ -4,10 +4,11 @@ import os
 import sys
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from adjust_split import adjust_for_splits
 from backward_dividend_adjust import backward_adjust_dividends
 
-def backfill(prod_backfill, prod, product_type):
+def backfill(prod_backfill, prod, product_type, plot_option):
     """
     Backfills open, close, high and low prices for ETFs which were created recently.
     Given input asset which is available when the ETF isn't it backfills the prices.
@@ -15,7 +16,7 @@ def backfill(prod_backfill, prod, product_type):
     Now the backward adjusted close price is used to backfill to keep daily log returns equal
     for both the assets.
 
-    If the assets are A and B and price of A when unavailable is being backfilld using price of B
+    If the assets are A and B and price of A when unavailable is being backfilled using price of B
     A_close_price_backfill_i/A_close_price_backfill_(i+1) = B_backward_adjusted_close_i/ B_backward_adjusted_close_(i+1)
     Asset prices of A where available are used without modification.
 
@@ -53,7 +54,7 @@ def backfill(prod_backfill, prod, product_type):
     starting_index = df_prod[df_prod['date']>=starting_date].index[0]
 
     #Print gap period in between them
-    print starting_date - df_prod['date'].iloc[starting_index] 
+    print "Gap period:" + str(starting_date - df_prod['date'].iloc[starting_index])
     
     # Add last last available enry in datafame to new dataframe 
     # This dataframe will store all backfilled  values
@@ -77,9 +78,21 @@ def backfill(prod_backfill, prod, product_type):
     df_backfilled['dividend'] = 0  # assume no dividends are being given out as backwad adjusted price used already 
     
     # Concat the backfilled dataframe and the available dataframe for prod_bf
-    df_prod_bf = pd.concat([df_backfilled.sort(['date'])[:-1],df_prod_bf])
+    df_prod_bf = pd.concat([df_backfilled.iloc[::-1][:-1],df_prod_bf])
     # Output to file
-    df_prod_bf.to_csv(output_path+prod_backfill+'_backfilled'+'.csv',index=False)    
+    df_prod_bf.to_csv(output_path+prod_backfill+'_backfilled'+'.csv',index=False)
+
+    #df_prod_bf = pd.read_csv(output_path+prod_backfill+'_backfilled'+'.csv')
+    if plot_option.lower() == 'y':
+        plot_path = '/home/debi/backfill/'
+        ax = df_prod.plot(y='backward_adjusted_close',use_index=False,legend=False)
+        patches1, labels1 = ax.get_legend_handles_labels()
+        ax2 = df_prod_bf.plot(y='close',ax=ax,use_index=False,secondary_y=True,legend=False)
+        patches2, labels2 = ax2.get_legend_handles_labels()
+        my_labels = [prod,prod_backfill+"(Right)"]
+        ax.legend(patches1 + patches2,my_labels,loc='best')
+        ax.axvline(x=starting_index)
+        plt.savefig(output_path+"plot_"+prod_backfill+"_"+prod+".png")
 
 
 def __main__() :
@@ -87,11 +100,12 @@ def __main__() :
         product_type = sys.argv[1]
         prod = sys.argv[2]
         prod_backfill = sys.argv[3]
+        plot_option = sys.argv[4]
     else:
-        print 'python backfill.py etf/fund product product_to_be_backfilled'
+        print 'python backfill.py etf/fund product product_to_be_backfilled plot(y/n)'
         sys.exit(0)
     
-    backfill(prod_backfill, prod, product_type)    
+    backfill(prod_backfill, prod, product_type, plot_option)    
 
 if __name__ == '__main__':
     __main__();
