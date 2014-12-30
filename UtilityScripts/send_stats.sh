5a86fce1e502e43ca3e610251866bfe1c3c4f055
Sanchit Gupta <sanchit.gupta@tworoads.co.in>
2014-12-30 13:35:40 +0000

    renamed UtilityScripts

--- UtilityScripts/send_stats.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-cd /home/cvdev/stratdev/
-python UtilityScripts/send_stats.py test/mails/IVWAS_all_rb1_tr10_std21.cfg
-python UtilityScripts/send_stats.py test/mails/TRERC_all_rb1_tr10_std63.2_corr252.30_maxiter100.cfg
-python UtilityScripts/send_stats.py test/mails/TRMSHC_all_rb5_tr10_std63.5_corr252.30.cfg
---
+++ utility_scripts/send_stats.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+cd /home/cvdev/stratdev/
+python utility_scripts/send_stats.py test/mails/IVWAS_all_rb1_tr10_std21.cfg
+python utility_scripts/send_stats.py test/mails/TRERC_all_rb1_tr10_std63.2_corr252.30_maxiter100.cfg
+python utility_scripts/send_stats.py test/mails/TRMSHC_all_rb5_tr10_std63.5_corr252.30.cfg
