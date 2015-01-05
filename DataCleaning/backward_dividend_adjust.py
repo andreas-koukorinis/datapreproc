11b018b3c7791b0afb514119cf50b1a4b05c97ce
debidatta <debi.me@gmail.com>
2015-01-05 17:30:55 +0530

    Added backward adjusted open and forwad adjusted open. Maintained previous ratio of open to close in both

--- DataCleaning/backward_dividend_adjust.py
+++ DataCleaning/backward_dividend_adjust.py
@@ -9,19 +9,25 @@ def backward_adjust_dividends( products, product_type ):
     for product in products:
         prices_file = path+product+'_split_adjusted.csv'
         df = pd.read_csv(prices_file,header=0)
+
         if product_type == 'etf':# If the product type is ETF
             df1 = df[ df.dividend > 0.0 ] # Select rows from dataframe in which the dividend was paid
             df['backward_adjusted_close'] = df['close'] # Make a new column and copy close prices initially
+            df['backward_adjusted_open'] = df['open'] # Make a new column and copy open prices initially
             for index, row in df1.iterrows(): # For each of the payouts
                 dividend_factor =  1 +  row['dividend'] / row['close'] # Calculate the dividend factor
                 df.loc[ (df.date < row['date']) ,'backward_adjusted_close'] /= dividend_factor # Divide all prices earlier to this payout by the dividend factor
+                df.loc[ (df.date < row['date']) ,'backward_adjusted_open'] /= dividend_factor # Divide all prices earlier to this payout by the dividend factor
 
         elif product_type == 'fund': # If the product type is MUTUAL FUND
             df1 = df[ df.dividend + df.capital_gain > 0.0 ] # Select rows from dataframe in which the dividend or capital gain was paid
             df['backward_adjusted_close'] = df['close'] # Make a new column and copy close prices initially
+            df['backward_adjusted_open'] = df['open'] # Make a new column and copy open prices initially
             for index, row in df1.iterrows(): # For each of the payouts               
                 dividend_factor = 1 + ( row['dividend'] + row['capital_gain'] ) / row['close'] # Calculate the dividend factor
                 df.loc[ (df.date < row['date']) ,'backward_adjusted_close'] /= dividend_factor # Divide all prices earlier to this payout by the dividend factor
+                df.loc[ (df.date < row['date']) ,'backward_adjusted_open'] /= dividend_factor # Divide all prices earlier to this payout by the dividend factor
+                
         df.to_csv(path+product+'_backward_dividend_adjusted'+'.csv',index=False) # Save result to csv 
 
 def __main__() :
