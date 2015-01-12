4038971ce91c8f81f488e1b6fd56a81c0e65d816
debidatta <debi.me@gmail.com>
2015-01-12 17:15:05 +0530

    Some directory changes to help Jenkins run

--- tests/test_simulator.py
+++ tests/test_simulator.py
@@ -3,32 +3,18 @@
 import os
 import sys
 
-sys.path.append(os.path.expanduser("~") + '/stratdev/')
-
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from utils.global_variables import Globals
 from simulator import Simulator
 
-def test_trvp_config1():
-    """ 
-    Testing TRVP with config1 
-    """
-    # Set config file path
-    _config_file = os.path.expanduser("~") + "/modeling/strategies/A1_TRVP_all_rb1_model1_rmp_profile1.cfg"
-    # Run simulator
-    sim1 = Simulator(_config_file)
-    sim1.run()
-    assert round(sim1._tradelogic_instance.performance_tracker.net_returns,2) == 161.08
-    assert round(sim1._tradelogic_instance.performance_tracker.sharpe,2) == 0.84
-    assert round(sim1._tradelogic_instance.performance_tracker.sortino,2) == 1.20
-
-
+directory = os.path.abspath("..")
 
 def test_mvo_config1():
     """ 
-    Testing TRVP with config1 
+    Testing MVO with config1 
     """
     # Set config file path
-    _config_file = os.path.expanduser("~") + "/modeling/strategies/A1_MVO_all_rb1_model1_rmp_profile1.cfg"
+    _config_file = directory + "/modeling/strategies/A1_MVO_all_rb1_model1_rmp_profile1.cfg"
 
     # Run simulator
     sim1 = Simulator(_config_file)
@@ -37,5 +23,15 @@ def test_mvo_config1():
     assert round(sim1._tradelogic_instance.performance_tracker.sharpe,2) == 0.87
     assert round(sim1._tradelogic_instance.performance_tracker.sortino,2) == 1.22
 
-
-
+def test_trvp_config1():
+    """ 
+    Testing TRVP with config1 
+    """
+    # Set config file path
+    _config_file = directory + "/modeling/strategies/A1_TRVP_all_rb1_model1_rmp_profile1.cfg"
+    # Run simulator
+    sim1 = Simulator(_config_file)
+    sim1.run()
+    assert round(sim1._tradelogic_instance.performance_tracker.net_returns,2) == 161.08
+    assert round(sim1._tradelogic_instance.performance_tracker.sharpe,2) == 0.84
+    assert round(sim1._tradelogic_instance.performance_tracker.sortino,2) == 1.20
