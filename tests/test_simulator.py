5f0b99f08cae3830c8e6a6115bcb88fd8fb87323
debidatta <debi.me@gmail.com>
2015-01-12 16:26:48 +0530

    reverting to correct test configuration

--- tests/test_simulator.py
+++ tests/test_simulator.py
@@ -8,14 +8,12 @@ sys.path.append(os.path.expanduser("~") + '/stratdev/')
 from utils.global_variables import Globals
 from simulator import Simulator
 
-
-def test_mvo_config1():
+def test_trvp_config1():
     """ 
     Testing TRVP with config1 
     """
     # Set config file path
-    _config_file = os.path.expanduser("~") + "/modeling/strategies/A1_MVO_all_rb1_model1_rmp_profile1.cfg"
-
+    _config_file = os.path.expanduser("~") + "/modeling/strategies/A1_TRVP_all_rb1_model1_rmp_profile1.cfg"
     # Run simulator
     sim1 = Simulator(_config_file)
     sim1.run()
@@ -38,3 +36,6 @@ def test_mvo_config1():
     assert round(sim1._tradelogic_instance.performance_tracker.net_returns,2) == 138.03
     assert round(sim1._tradelogic_instance.performance_tracker.sharpe,2) == 0.87
     assert round(sim1._tradelogic_instance.performance_tracker.sortino,2) == 1.22
+
+
+
