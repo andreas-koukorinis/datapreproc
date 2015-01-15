40d21f15a6d5343969249a61afa858cc3f8ff9e6
Sanchit Gupta <sanchit.gupta@tworoads.co.in>
2015-01-15 11:27:19 +0000

    got replaced somehow by merge

--- data_cleaning/nanex_scripts/check_nanex_upload_s3.sh
+++ data_cleaning/nanex_scripts/check_nanex_upload_s3.sh
@@ -2,7 +2,7 @@
 
 cd /home/cvdev/stratdev/data_cleaning/nanex_scripts/
 
-#fname="20141103.YC.nxc"
+fname="`date +%Y%m%d -d "yesterday"`.YC.nxc"
 var='s3cmd ls s3://cvquantdata/nanex/rawdata/'
 var1=`$var$fname`
 echo $var1
@@ -10,4 +10,6 @@ if [ -n "$var1" ]; then
     mail -s "Nanex tape successfully uploaded on `date +%Y%m%d -d "yesterday"`" "sanchit.gupta@tworoads.co.in" <<< ""
 else
     mail -s "Alert! Nanex tape not uploaded on `date +%Y%m%d -d "yesterday"`" "sanchit.gupta@tworoads.co.in" <<< ""
+    cd /apps/data/nanex/
+    nohup ./NxD_Circulumvite 1 60 2 > nohup.out
 fi
