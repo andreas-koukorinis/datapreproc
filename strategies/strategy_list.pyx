14b150ef6c65dbe215bd41bc18c9d28fc2ec10a9
Sanchit Gupta <sanchit.gupta@tworoads.co.in>
2015-01-17 10:34:12 +0000

    added rank of sharpe based aggregator

--- strategies/strategy_list.pyx
+++ strategies/strategy_list.pyx
@@ -1,6 +1,6 @@
 # cython: profile=True
 def is_valid_strategy_name ( _strategy_name ) :
-    _strategy_name_list = ['AggregatorCWAS', 'AggregatorIV']
+    _strategy_name_list = ['AggregatorCWAS', 'AggregatorIV', 'AggregatorSharpe']
     _retval = False
     if _strategy_name in _strategy_name_list :
         _retval = True
@@ -13,9 +13,12 @@ def get_module_name_from_strategy_name ( strategy_name ) :
         return ( "aggregator_cwas" )
     def _aggregator_iv():
         return ( "aggregator_iv" )
+    def _aggregator_sharpe():
+        return ( "aggregator_sharpe" )
 
     options = { 'AggregatorCWAS' : _aggregator_cwas,
-                'AggregatorIV' : _aggregator_iv
+                'AggregatorIV' : _aggregator_iv,
+                'AggregatorSharpe' : _aggregator_sharpe
                 }
 
     if is_valid_strategy_name ( strategy_name ):
