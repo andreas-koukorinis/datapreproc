9edc093a7ba7698d480bdd026277346dca34cdbd
Sanchit Gupta <sanchit.gupta@tworoads.co.in>
2015-02-10 13:10:58 +0000

    ~

--- data_cleaning/csi_scripts/daily_csi_dump_db.sh
+++ data_cleaning/csi_scripts/daily_csi_dump_db.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 cd /home/cvdev/stratdev/data_cleaning/csi_scripts/
 echo "DB DUMP CSI DAT FOR ETF's AND FUNDS STATUS:\n" >> /apps/logs/log_`date +%Y%m%d -d "yesterday"`
-#COUNTER=92
-#>log
-#while [  $COUNTER -gt 0 ]; do
-#    echo The counter is $COUNTER
-#    python daily_update.py futures $COUNTER TU FV TY US NK NIY ES EMD NQ YM AD BP CD CU1 JY MP NE2 SF GC SI HG PL PA LH ZW ZC ZS ZM ZL EBS EBM EBL SXE FDX SMI SXF CGB FFI FLG AEX KC CT CC SB JTI JGB JNI SIN SSG HCE HSI ALS YAP MFX KOS JPYUSD CADUSD GBPUSD EURUSD AUDUSD NZDUSD CHFUSD SEKUSD NOKUSD TRYUSD MXNUSD ZARUSD ILSUSD SGDUSD HKDUSD TWDUSD >> log 
+COUNTER=101
+>log
+while [  $COUNTER -gt 0 ]; do
+    echo The counter is $COUNTER
+    python daily_update.py us-stocks $COUNTER VTI VTV VBR VEA VWO VXUS VT IEMG BND MUB LQD VWOB SHV >> log
+    python daily_update.py funds $COUNTER AQRIX AQMIX QGMIX SRPFX ABRZX VBLTX VTSMX >> log
     #echo "python daily_update.py futures $COUNTER TU"
-#    let COUNTER=COUNTER-1 
-#done
-python daily_update.py futures 1 TU FV TY US NK NIY ES EMD NQ YM AD BP CD CU1 JY MP NE2 SF GC SI HG PL PA LH ZW ZC ZS ZM ZL EBS EBM EBL SXE FDX SMI SXF CGB FFI FLG AEX KC CT CC SB JTI JGB JNI SIN SSG HCE HSI ALS YAP MFX KOS JPYUSD CADUSD GBPUSD EURUSD AUDUSD NZDUSD CHFUSD SEKUSD NOKUSD TRYUSD MXNUSD ZARUSD ILSUSD SGDUSD HKDUSD TWDUSD >> /apps/logs/log_`date +%Y%m%d -d "yesterday"`
+    let COUNTER=COUNTER-1 
+done
+# VOE VWO TIP VTIP BNDX
+#python daily_update.py futures 1 TU FV TY US NK NIY ES EMD NQ YM AD BP CD CU1 JY MP NE2 SF GC SI HG PL PA LH ZW ZC ZS ZM ZL EBS EBM EBL SXE FDX SMI SXF CGB FFI FLG AEX KC CT CC SB JTI JGB JNI SIN SSG HCE HSI ALS YAP MFX KOS JPYUSD CADUSD GBPUSD EURUSD AUDUSD NZDUSD CHFUSD SEKUSD NOKUSD TRYUSD MXNUSD ZARUSD ILSUSD SGDUSD HKDUSD TWDUSD >> /apps/logs/log_`date +%Y%m%d -d "yesterday"`
 
-python daily_update.py us-stocks 1 VTI VTV VOE VBR VEA VWO VXUS VT IEMG TIP VTIP BND MUB LQD BNDX VWOB SHV >> /apps/logs/log_`date +%Y%m%d -d "yesterday"`
-python daily_update.py funds 1 AQRIX AQMIX QGMIX SRPFX ABRZX VBLTX VTSMX>> /apps/logs/log_`date +%Y%m%d -d "yesterday"`
+#python daily_update.py us-stocks 1 VTI VTV VBR VEA VWO VXUS VT IEMG BND MUB LQD VWOB SHV >> /apps/logs/log_`date +%Y%m%d -d "yesterday"`
+#python daily_update.py funds 1 AQRIX AQMIX QGMIX SRPFX ABRZX VBLTX VTSMX>> /apps/logs/log_`date +%Y%m%d -d "yesterday"`
