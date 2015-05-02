1e0e27e71df52b16f52ddda1a91415583039d2ab
Sanchit Gupta <sanchit.gupta@tworoads.co.in>
2015-05-02 13:50:08 +0000

    script to run on csv trade files that mansi would send

---
+++ data_cleaning/wedbush/update_actual_trades.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+import sys
+import os
+import argparse
+import smtplib
+import MySQLdb
+import pandas as pd
+from datetime import datetime, date, timedelta
+home_path = os.path.expanduser("~")
+sys.path.append(home_path + '/stratdev/')
+from utils.dbqueries import connect_to_db, get_latest_currency_and_conversion_factors, fetch_latest_prices
+
+order_shortcode_map = {'EP':'ES','CA6':'6C','TYA':'ZN','DSX':'FESX','QGA':'LFR'}
+cqg_multiplier_map = {'ES': 100.0, '6C': 10000.0, 'ZN' : 1000.0, 'FESX' : 10.0, 'LFR' : 100.0} # TODO looks like this can change
+
+def send_mail( err, msg ):
+    server = smtplib.SMTP( "localhost" )
+    server.sendmail("sanchit.gupta@tworoads.co.in", "sanchit.gupta@tworoads.co.in;debidatta.dwibedi@tworoads.co.in", 'EXCEPTION %s %s' % ( err, msg ) )
+
+#'4/30/2015 11:59'
+def parse_dt ( dt_str ):
+    dt = datetime.strptime(dt_str, "%m/%d/%Y %H:%M")
+    return dt.date(), dt.strftime( '%H:%M')
+
+def main():
+    # Parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('order_file')
+    parser.add_argument('-t', type=str, help='Type  for products being ETFs\nEg: -t etf\n Default is future i.e. trading futures',default='future', dest='product_type')
+    args = parser.parse_args()
+    product_type = args.product_type
+    
+    # Connect to db
+    try:
+        db = connect_to_db("fixed-income1.clmdxgxhslqn.us-east-1.rds.amazonaws.com", "live_trading", 'w')
+        db_cursor = db.cursor(MySQLdb.cursors.DictCursor)
+    except Exception, err:
+        send_mail( err, 'Could not connect to db' )
+
+    # Update `date`,`product`, `place_time`, `fill_time`, `place_amount`, `fill_amount`, `order_price`, `fill_price`, `commission`
+    order_df = pd.DataFrame.from_csv( args.order_file, index_col='Order #' )
+    try:
+        for fc in order_df.index:
+            place_dt_str = order_df.loc[fc]['Place Time']
+            place_date, place_time = parse_dt( place_dt_str )
+            fill_dt_str = order_df.loc[fc]['Fill Time']
+            fill_date, fill_time = parse_dt( fill_dt_str )
+            order_symbol = str( order_df.loc[fc]['Symbol'][:-2] )
+            basename = order_shortcode_map[order_symbol]  
+            exchange_symbol = basename + order_df.loc[fc]['Symbol'][-2] + place_date.strftime('%y')
+            buy_or_sell = order_df.loc[fc]['B/S']
+            sign_amount = 1 if buy_or_sell == 'Sell' else -1
+            place_amount = order_df.loc[fc]['Qty'] * sign_amount
+            fill_amount = order_df.loc[fc]['Filled'] * sign_amount
+            order_price = order_df.loc[fc]['Order Price']/cqg_multiplier_map[basename]
+            fill_price = order_df.loc[fc]['Fill Price']/cqg_multiplier_map[basename]
+            #print place_date, exchange_symbol, place_time, fill_time, place_amount, fill_amount, order_price, fill_price
+            try:
+                query = "INSERT INTO actual_orders (date, product, place_time, fill_time, place_amount, fill_amount, order_price, fill_price ) VALUES('%s','%s','%s','%s','%d', '%d', '%s', '%s')" \
+                        % ( place_date, exchange_symbol, place_time, fill_time, place_amount, fill_amount, order_price, fill_price )
+                db_cursor.execute(query)
+                db.commit()
+            except Exception, err:
+                db.rollback()
+                send_mail( err, 'Could not insert order into db' )
+
+    except Exception, err:
+        send_mail( err, 'Some issue in reading order file' )
+
+if __name__ == '__main__':
+    main()
