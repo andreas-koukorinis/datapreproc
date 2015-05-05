88abcac6131f1291c1241f6247b08e3b2105c17b
Sanchit Gupta <sanchit.gupta@tworoads.co.in>
2015-05-05 09:09:44 +0000

    python use 2.7

--- data_cleaning/csi_scripts/daily_csi_dump_db.sh
+++ data_cleaning/csi_scripts/daily_csi_dump_db.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+source /apps/pythonenv/py2.7/bin/activate
+
 #cd /home/cvdev/stratdev/data_cleaning/csi_scripts/
 cd /home/cvdev/datapreproc/data_cleaning/csi_scripts/
 echo "DB DUMP CSI DAT FOR ETF's AND FUNDS STATUS:\n" >> /apps/logs/log_`date +%Y%m%d -d "yesterday"`
