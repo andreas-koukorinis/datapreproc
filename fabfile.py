74d01f07994cff2a391c11f227991502faab6b63
Sanchit Gupta <sanchit.gupta@tworoads.co.in>
2015-06-29 12:53:05 +0000

    corrected ssh key

--- fabfile.py
+++ fabfile.py
@@ -2,12 +2,12 @@ from fabric.api import *
 
 env.roledefs = {'workers':['54.164.22.228','54.172.28.157']}
 env.user = 'cvdev'
-env.key_filename = '/home/cvdev/.ssh/cvdev_rsa'
+env.key_filename = '/home/cvdev/.ssh/cvdev.pem'
 
 def update_code():
     code_dir = '/home/cvdev/datapreproc/'
     with cd(code_dir):
-        run('git pull origin master')
+        run('git pull origin master', shell=False)
 
 def update_stratdev():
     code_dir = '/home/cvdev/stratdev/'
@@ -20,7 +20,9 @@ def update_modeling():
         run('git pull origin master')
 
 def restart_worker():
-	code_dir = '/home/cvdev/datapreproc/'
+    code_dir = '/home/cvdev/datapreproc/'
+    env.user = 'ec2-user'
+    env.key_filename = '/home/cvdev/.ssh/cvfif1.pem'
     with cd(code_dir):
-        run('source /apps/pythonenv/py2.7/bin/activate')
-        run('supervisorctl restart celery-worker')
+        sudo('source /apps/pythonenv/py2.7/bin/activate', shell=False, user=root)
+        sudo('supervisorctl restart celery-worker', shell=False, user=root)
