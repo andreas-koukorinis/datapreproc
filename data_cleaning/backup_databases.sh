d50335ef27d5cf325cafa256ec7b54eea949a1d6
Sanchit Gupta <sanchit.gupta@tworoads.co.in>
2015-07-02 12:40:22 +0000

    log backup status

--- data_cleaning/backup_databases.sh
+++ data_cleaning/backup_databases.sh
@@ -5,11 +5,15 @@ uname_upass_arr=(${uname_upass//:/ })
 uname=${uname_upass_arr[0]}
 upass=${uname_upass_arr[1]}
 
+
 days_to_keep=7
 database_dump_path='/spare/local/database_dumps/'
 database_to_backup=( daily_qplum live_trading risk_parity simula webapp workbench )
 today_date=`date +%Y%m%d`
 older_date=`date -d "$days_to_keep day ago" +%Y%m%d`
+
+>/apps/logs/database_dumps/log_$today_date
+
 for database in ${database_to_backup[@]}
 do
   # Notify
@@ -19,12 +23,17 @@ do
   older_dump_file=$database_dump_path$database'_'$older_date'.sql'
 
   # Dump this database
-  `mysqldump -h fixed-income1.clmdxgxhslqn.us-east-1.rds.amazonaws.com -u $uname -p$upass $database > $today_dump_file` 
+  error_msg=`mysqldump -h fixed-income1.clmdxgxhslqn.us-east-1.rds.amazonaws.com -u $uname -p$upass $database > $today_dump_file` 
 
   # Remove old dumps
   `rm -rf $older_dump_file`
-
+ 
   # Notify
-  echo $database' backed up successfully'
+  if [ -f $today_dump_file ];
+  then
+    echo $database' backed up successfully'
+  else
+    echo $database' backup failed\n'$error_msg
+  fi
 done
 
